9632b07c136475a0f43631ea50c0e8695c5de387
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:54 +0200

    Added windows/ms_dtyp/common/data_types/DWORD32.go

---
+++ windows/ms_dtyp/common/data_types/DWORD32.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A DWORD32 is a 32-bit unsigned integer.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/e359fcc1-35c6-4bcd-964d-d59927aeea98
+type DWORD32 = uint32
