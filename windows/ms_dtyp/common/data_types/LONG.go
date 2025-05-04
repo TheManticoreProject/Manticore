e1ed55453dfa387dfe7dd6f4949b9bc2e662e081
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:22 +0200

    Added windows/ms_dtyp/common/data_types/LONG.go

---
+++ windows/ms_dtyp/common/data_types/LONG.go
@@ -0,0 +1,6 @@
+package data_types
+
+// A LONG is a 32-bit signed integer, in twos-complement format (range: -2147483648 through 2147483647 decimal).
+// The first bit (Most Significant Bit (MSB)) is the signing bit.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/29d44d70-382f-4998-9d76-8a1fe93e445c
+type LONG = int32
