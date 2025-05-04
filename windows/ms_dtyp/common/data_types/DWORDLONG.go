f51ee2df2cb4779d6620ecbcd7a9e661a34ca19e
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:23 +0200

    Added windows/ms_dtyp/common/data_types/DWORDLONG.go

---
+++ windows/ms_dtyp/common/data_types/DWORDLONG.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A DWORDLONG is a 64-bit unsigned integer (range: 0 through 18446744073709551615 decimal).
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/c575fb47-166c-48cd-a37c-e44cac05c3d6
+type DWORDLONG = uint64
