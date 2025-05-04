e3f8d709712a970e18c4753a8ea3d575d9cc90fc
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:08 +0200

    Added windows/ms_dtyp/common/data_types/ULONG_PTR.go

---
+++ windows/ms_dtyp/common/data_types/ULONG_PTR.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A ULONG_PTR is an unsigned long type used for pointer precision. It is used when casting a pointer to a long type to perform pointer arithmetic.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/21eec394-630d-49ed-8b4a-ab74a1614611
+type ULONG_PTR = uintptr
