914729b8ad7601a76a86ce868e8c06957904134c
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:49 +0200

    Added windows/ms_dtyp/common/data_types/LONGLONG.go

---
+++ windows/ms_dtyp/common/data_types/LONGLONG.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A LONGLONG is a 64-bit signed integer (range: -9223372036854775808 through 9223372036854775807 decimal).
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/e34f8048-e2d0-4f5b-99fd-0b6489ee0295
+type LONGLONG = int64
