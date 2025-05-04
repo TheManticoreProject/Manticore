2ce0e6d6e4268ac52d37d2c92d646e88d28fb110
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:15 +0200

    Added windows/ms_dtyp/common/data_types/BOOLEAN.go

---
+++ windows/ms_dtyp/common/data_types/BOOLEAN.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A BOOLEAN is an 8-bit field that is set to 1 to indicate TRUE, or 0 to indicate FALSE.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/51bbfbb1-08e2-4c13-a95e-1eaa7d310670
+type BOOLEAN = uint8
