15525ae893866b3a180ea1b4a90a6dba8b3faf29
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:48 +0200

    Added windows/ms_dtyp/common/data_types/UINT16.go

---
+++ windows/ms_dtyp/common/data_types/UINT16.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A UINT16 is a 16-bit unsigned integer (range: 0 through 65535 decimal). Because a UINT16 is unsigned, its first bit (Most Significant Bit (MSB)) is not reserved for signing.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/6c1609ad-8592-45d3-9dfa-38e6a30ff203
+type UINT16 = uint16
