ceee4a93e4ecc04104b812518ca181c153087314
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:56:54 +0200

    Added network/smb/smb_v10/message/commands/Transaction2SecondaryResponse.go

---
+++ network/smb/smb_v10/message/commands/Transaction2SecondaryResponse.go
@@ -0,0 +1,130 @@
+package commands
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/data"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/parameters"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/types"
+)
+
+// Transaction2SecondaryResponse
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/b0028be1-5325-4fef-8659-341524362431
+type Transaction2SecondaryResponse struct {
+	command_interface.Command
+
+	// Parameters
+
+	// Data
+
+}
+
+// NewTransaction2SecondaryResponse creates a new Transaction2SecondaryResponse structure
+//
+// Returns:
+// - A pointer to the new Transaction2SecondaryResponse structure
+func NewTransaction2SecondaryResponse() *Transaction2SecondaryResponse {
+	c := &Transaction2SecondaryResponse{
+		// Parameters
+
+		// Data
+
+	}
+
+	c.Command.SetCommandCode(codes.SMB_COM_TRANSACTION2_SECONDARY)
+
+	return c
+}
+
+
+
+// Marshal marshals the Transaction2SecondaryResponse structure into a byte array
+//
+// Returns:
+// - A byte array representing the Transaction2SecondaryResponse structure
+// - An error if the marshaling fails
+func (c *Transaction2SecondaryResponse) Marshal() ([]byte, error) {
+	marshalledCommand := []byte{}
+
+	// Create the Parameters structure if it is nil
+	if c.GetParameters() == nil {
+		c.SetParameters(parameters.NewParameters())
+	}
+	// Create the Data structure if it is nil
+	if c.GetData() == nil {
+		c.SetData(data.NewData())
+	}
+
+	// In case of AndX, we need to add the parameters to the Parameters structure first
+	if c.IsAndX() {
+		if c.GetAndX() == nil {
+			c.SetAndX(andx.NewAndX())
+			c.GetAndX().AndXCommand = c.GetCommandCode()
+		}
+
+		for _, parameter := range c.GetAndX().GetParameters() {
+			c.GetParameters().AddWord(parameter)
+		}
+	}
+
+	// First marshal the data and then the parameters
+	// This is because some parameters are dependent on the data, for example the size of some fields within
+	// the data will be stored in the parameters
+	rawDataContent := []byte{}
+	
+	// Then marshal the parameters
+	rawParametersContent := []byte{}
+	
+	// Marshalling parameters
+	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
+	marshalledParameters, err := c.GetParameters().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledParameters...)
+	
+	// Marshalling data
+	c.GetData().Add(rawDataContent)
+	marshalledData, err := c.GetData().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledData...)
+
+	return marshalledCommand, nil
+}
+
+// Unmarshal unmarshals a byte array into the command structure
+//
+// Parameters:
+// - data: The byte array to unmarshal
+//
+// Returns:
+// - The number of bytes unmarshalled
+func (c *Transaction2SecondaryResponse) Unmarshal(data []byte) (int, error) {
+	offset := 0
+
+	// First unmarshal the two structures
+	bytesRead, err := c.GetParameters().Unmarshal(data)
+	if err != nil {
+		return 0, err
+	}
+	rawParametersContent := c.GetParameters().GetBytes()
+	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	if err != nil {
+		return 0, err
+	}
+	rawDataContent := c.GetData().GetBytes()
+
+	// First unmarshal the parameters
+	offset = 0
+	
+	// Then unmarshal the data
+	offset = 0
+
+	return offset, nil
+}
