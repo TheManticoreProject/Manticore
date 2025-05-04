c0e5a38be5b90430ea28813929290e75ea26dd05
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:40 +0200

    Added windows/ms_dtyp/common/data_types/CHAR.go

---
+++ windows/ms_dtyp/common/data_types/CHAR.go
@@ -0,0 +1,6 @@
+package data_types
+
+// A CHAR is an 8-bit block of data that typically contains an ANSI character, as specified in [ISO/IEC-8859-1].
+// For information on the char keyword, see [C706] section 4.2.9.3.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/77e1033f-b31d-4bd2-b3d5-9f3c9faa22eb
+type CHAR = byte
