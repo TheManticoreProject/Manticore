dc94ec5804ca4a0f22de5fd688e36f4502d4fbe9
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:25 +0200

    Added windows/ms_dtyp/common/data_types/WCHAR.go

---
+++ windows/ms_dtyp/common/data_types/WCHAR.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A WCHAR is a 16-bit Unicode character.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/7df7c1d5-492c-4db4-a992-5cd9e887c5d7
+type WCHAR = UNICODE
