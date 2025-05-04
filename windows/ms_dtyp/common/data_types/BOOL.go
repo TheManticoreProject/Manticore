f9c7060b71f040724ccc005a7323dcbd40f1e14f
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:07 +0200

    Added windows/ms_dtyp/common/data_types/BOOL.go

---
+++ windows/ms_dtyp/common/data_types/BOOL.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A BOOL is a 32-bit field that is set to 1 to indicate TRUE, or 0 to indicate FALSE
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/9d81be47-232e-42cf-8f0d-7a3b29bf2eb2
+type BOOL = uint32
