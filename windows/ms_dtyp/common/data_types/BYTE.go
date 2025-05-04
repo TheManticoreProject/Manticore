09f9a1a8b0c0c415029069e729c95535dd56b5d4
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:53 +0200

    Added windows/ms_dtyp/common/data_types/BYTE.go

---
+++ windows/ms_dtyp/common/data_types/BYTE.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A BYTE is an 8-bit unsigned value that corresponds to a single octet in a network protocol.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/d7edc080-e499-4219-a837-1bc40b64bb04
+type BYTE = UCHAR
