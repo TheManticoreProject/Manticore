f4dee4b398f53c5184e1f361b89da2ed97e5b868
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:57:27 +0200

    Added network/smb/smb_v10/message/commands/ReadAndxResponse.go

---
+++ network/smb/smb_v10/message/commands/ReadAndxResponse.go
@@ -0,0 +1,256 @@
+package commands
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/data"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/parameters"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/types"
+)
+
+// ReadAndxResponse
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/89d6b552-5406-445c-85d5-54c80b94a20f
+type ReadAndxResponse struct {
+	command_interface.Command
+
+	// Parameters
+	WordCount types.UCHAR
+	AndXCommand types.UCHAR
+	AndXReserved types.UCHAR
+	AndXOffset types.USHORT
+	Available types.USHORT
+	DataCompactionMode types.USHORT
+	Reserved1 types.USHORT
+	DataLength types.USHORT
+	DataOffset types.USHORT
+
+	// Data
+
+}
+
+// NewReadAndxResponse creates a new ReadAndxResponse structure
+//
+// Returns:
+// - A pointer to the new ReadAndxResponse structure
+func NewReadAndxResponse() *ReadAndxResponse {
+	c := &ReadAndxResponse{
+		// Parameters
+		WordCount: types.UCHAR(0),
+		AndXCommand: types.UCHAR(0),
+		AndXReserved: types.UCHAR(0),
+		AndXOffset: types.USHORT(0),
+		Available: types.USHORT(0),
+		DataCompactionMode: types.USHORT(0),
+		Reserved1: types.USHORT(0),
+		DataLength: types.USHORT(0),
+		DataOffset: types.USHORT(0),
+
+		// Data
+
+	}
+
+	c.Command.SetCommandCode(codes.SMB_COM_READ_ANDX)
+
+	return c
+}
+
+
+// IsAndX returns true if the command is an AndX
+func (c *ReadAndxResponse) IsAndX() bool {
+	return true
+}
+
+
+
+// Marshal marshals the ReadAndxResponse structure into a byte array
+//
+// Returns:
+// - A byte array representing the ReadAndxResponse structure
+// - An error if the marshaling fails
+func (c *ReadAndxResponse) Marshal() ([]byte, error) {
+	marshalledCommand := []byte{}
+
+	// Create the Parameters structure if it is nil
+	if c.GetParameters() == nil {
+		c.SetParameters(parameters.NewParameters())
+	}
+	// Create the Data structure if it is nil
+	if c.GetData() == nil {
+		c.SetData(data.NewData())
+	}
+
+	// In case of AndX, we need to add the parameters to the Parameters structure first
+	if c.IsAndX() {
+		if c.GetAndX() == nil {
+			c.SetAndX(andx.NewAndX())
+			c.GetAndX().AndXCommand = c.GetCommandCode()
+		}
+
+		for _, parameter := range c.GetAndX().GetParameters() {
+			c.GetParameters().AddWord(parameter)
+		}
+	}
+
+	// First marshal the data and then the parameters
+	// This is because some parameters are dependent on the data, for example the size of some fields within
+	// the data will be stored in the parameters
+	rawDataContent := []byte{}
+	
+	// Then marshal the parameters
+	rawParametersContent := []byte{}
+	
+	// Marshalling parameter WordCount
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
+	
+	// Marshalling parameter AndXCommand
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.AndXCommand))
+	
+	// Marshalling parameter AndXReserved
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.AndXReserved))
+	
+	// Marshalling parameter AndXOffset
+	buf2 := make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.AndXOffset))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter Available
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.Available))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter DataCompactionMode
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.DataCompactionMode))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter Reserved1
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.Reserved1))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter DataLength
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.DataLength))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter DataOffset
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.DataOffset))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameters
+	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
+	marshalledParameters, err := c.GetParameters().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledParameters...)
+	
+	// Marshalling data
+	c.GetData().Add(rawDataContent)
+	marshalledData, err := c.GetData().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledData...)
+
+	return marshalledCommand, nil
+}
+
+// Unmarshal unmarshals a byte array into the command structure
+//
+// Parameters:
+// - data: The byte array to unmarshal
+//
+// Returns:
+// - The number of bytes unmarshalled
+func (c *ReadAndxResponse) Unmarshal(data []byte) (int, error) {
+	offset := 0
+
+	// First unmarshal the two structures
+	bytesRead, err := c.GetParameters().Unmarshal(data)
+	if err != nil {
+		return 0, err
+	}
+	rawParametersContent := c.GetParameters().GetBytes()
+	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	if err != nil {
+		return 0, err
+	}
+	rawDataContent := c.GetData().GetBytes()
+
+	// First unmarshal the parameters
+	offset = 0
+	
+	// Unmarshalling parameter WordCount
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for WordCount")
+	}
+	c.WordCount = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter AndXCommand
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for AndXCommand")
+	}
+	c.AndXCommand = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter AndXReserved
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for AndXReserved")
+	}
+	c.AndXReserved = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter AndXOffset
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for AndXOffset")
+	}
+	c.AndXOffset = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter Available
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for Available")
+	}
+	c.Available = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter DataCompactionMode
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for DataCompactionMode")
+	}
+	c.DataCompactionMode = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter Reserved1
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for Reserved1")
+	}
+	c.Reserved1 = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter DataLength
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for DataLength")
+	}
+	c.DataLength = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter DataOffset
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for DataOffset")
+	}
+	c.DataOffset = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Then unmarshal the data
+	offset = 0
+
+	return offset, nil
+}
