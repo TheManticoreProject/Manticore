d0a2a56cdb42e5d845fa2ab540449b58762cd5d6
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:57:01 +0200

    Added network/smb/smb_v10/message/commands/RenameRequest.go

---
+++ network/smb/smb_v10/message/commands/RenameRequest.go
@@ -0,0 +1,204 @@
+package commands
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/data"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/parameters"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/types"
+)
+
+// RenameRequest
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/c970f3bf-806e-4309-8ea9-6515605f450d
+type RenameRequest struct {
+	command_interface.Command
+
+	// Parameters
+	WordCount types.UCHAR
+	SearchAttributes types.SMB_FILE_ATTRIBUTES
+
+	// Data
+	BufferFormat1 types.UCHAR
+	OldFileName types.SMB_STRING
+	BufferFormat2 types.UCHAR
+	NewFileName types.SMB_STRING
+
+}
+
+// NewRenameRequest creates a new RenameRequest structure
+//
+// Returns:
+// - A pointer to the new RenameRequest structure
+func NewRenameRequest() *RenameRequest {
+	c := &RenameRequest{
+		// Parameters
+		WordCount: types.UCHAR(0),
+		SearchAttributes: types.SMB_FILE_ATTRIBUTES{},
+
+		// Data
+		BufferFormat1: types.UCHAR(0),
+		OldFileName: types.SMB_STRING{},
+		BufferFormat2: types.UCHAR(0),
+		NewFileName: types.SMB_STRING{},
+
+	}
+
+	c.Command.SetCommandCode(codes.SMB_COM_RENAME)
+
+	return c
+}
+
+
+
+// Marshal marshals the RenameRequest structure into a byte array
+//
+// Returns:
+// - A byte array representing the RenameRequest structure
+// - An error if the marshaling fails
+func (c *RenameRequest) Marshal() ([]byte, error) {
+	marshalledCommand := []byte{}
+
+	// Create the Parameters structure if it is nil
+	if c.GetParameters() == nil {
+		c.SetParameters(parameters.NewParameters())
+	}
+	// Create the Data structure if it is nil
+	if c.GetData() == nil {
+		c.SetData(data.NewData())
+	}
+
+	// In case of AndX, we need to add the parameters to the Parameters structure first
+	if c.IsAndX() {
+		if c.GetAndX() == nil {
+			c.SetAndX(andx.NewAndX())
+			c.GetAndX().AndXCommand = c.GetCommandCode()
+		}
+
+		for _, parameter := range c.GetAndX().GetParameters() {
+			c.GetParameters().AddWord(parameter)
+		}
+	}
+
+	// First marshal the data and then the parameters
+	// This is because some parameters are dependent on the data, for example the size of some fields within
+	// the data will be stored in the parameters
+	rawDataContent := []byte{}
+	
+	// Marshalling data BufferFormat1
+	rawDataContent = append(rawDataContent, types.UCHAR(c.BufferFormat1))
+	
+	// Marshalling data OldFileName
+	bytesStream, err := c.OldFileName.Marshal()
+	if err != nil {
+			return nil, err
+	}
+	rawDataContent = append(rawDataContent, bytesStream...)
+	
+	// Marshalling data BufferFormat2
+	rawDataContent = append(rawDataContent, types.UCHAR(c.BufferFormat2))
+	
+	// Marshalling data NewFileName
+	bytesStream, err := c.NewFileName.Marshal()
+	if err != nil {
+			return nil, err
+	}
+	rawDataContent = append(rawDataContent, bytesStream...)
+	
+	// Then marshal the parameters
+	rawParametersContent := []byte{}
+	
+	// Marshalling parameter WordCount
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
+	
+	// Marshalling parameter SearchAttributes
+	
+	// Marshalling parameters
+	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
+	marshalledParameters, err := c.GetParameters().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledParameters...)
+	
+	// Marshalling data
+	c.GetData().Add(rawDataContent)
+	marshalledData, err := c.GetData().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledData...)
+
+	return marshalledCommand, nil
+}
+
+// Unmarshal unmarshals a byte array into the command structure
+//
+// Parameters:
+// - data: The byte array to unmarshal
+//
+// Returns:
+// - The number of bytes unmarshalled
+func (c *RenameRequest) Unmarshal(data []byte) (int, error) {
+	offset := 0
+
+	// First unmarshal the two structures
+	bytesRead, err := c.GetParameters().Unmarshal(data)
+	if err != nil {
+		return 0, err
+	}
+	rawParametersContent := c.GetParameters().GetBytes()
+	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	if err != nil {
+		return 0, err
+	}
+	rawDataContent := c.GetData().GetBytes()
+
+	// First unmarshal the parameters
+	offset = 0
+	
+	// Unmarshalling parameter WordCount
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for WordCount")
+	}
+	c.WordCount = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter SearchAttributes
+	
+	// Then unmarshal the data
+	offset = 0
+	
+	// Unmarshalling data BufferFormat1
+	if len(rawDataContent) < offset+1 {
+	    return offset, fmt.Errorf("rawParametersContent too short for BufferFormat1")
+	}
+	c.BufferFormat1 = types.UCHAR(rawDataContent[offset])
+	offset++
+	
+	// Unmarshalling data OldFileName
+	bytesRead, err := c.OldFileName.Unmarshal(rawDataContent[offset:])
+	if err != nil {
+	    return offset, err
+	}
+	offset += bytesRead
+	
+	// Unmarshalling data BufferFormat2
+	if len(rawDataContent) < offset+1 {
+	    return offset, fmt.Errorf("rawParametersContent too short for BufferFormat2")
+	}
+	c.BufferFormat2 = types.UCHAR(rawDataContent[offset])
+	offset++
+	
+	// Unmarshalling data NewFileName
+	bytesRead, err := c.NewFileName.Unmarshal(rawDataContent[offset:])
+	if err != nil {
+	    return offset, err
+	}
+	offset += bytesRead
+
+	return offset, nil
+}
