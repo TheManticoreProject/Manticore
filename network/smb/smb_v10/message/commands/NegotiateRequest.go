3b4e7ba81b41a905d93e8a4300cd865381d1fa14
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:56:35 +0200

    Added network/smb/smb_v10/message/commands/NegotiateRequest.go

---
+++ network/smb/smb_v10/message/commands/NegotiateRequest.go
@@ -0,0 +1,154 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/dialects"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/data"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/parameters"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/types"
+)
+
+// NegotiateRequest
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/25c8c3c9-58fc-4bb8-aa8f-0272dede84c5
+type NegotiateRequest struct {
+	command_interface.Command
+
+	// Parameters
+	WordCount types.UCHAR
+
+	// Data
+	Dialects dialects.Dialects
+}
+
+// NewNegotiateRequest creates a new NegotiateRequest structure
+//
+// Returns:
+// - A pointer to the new NegotiateRequest structure
+func NewNegotiateRequest() *NegotiateRequest {
+	c := &NegotiateRequest{
+		// Parameters
+		WordCount: types.UCHAR(0),
+
+		// Data
+		Dialects: dialects.Dialects{},
+	}
+
+	c.Command.SetCommandCode(codes.SMB_COM_NEGOTIATE)
+
+	return c
+}
+
+// Marshal marshals the NegotiateRequest structure into a byte array
+//
+// Returns:
+// - A byte array representing the NegotiateRequest structure
+// - An error if the marshaling fails
+func (c *NegotiateRequest) Marshal() ([]byte, error) {
+	marshalledCommand := []byte{}
+
+	// Create the Parameters structure if it is nil
+	if c.GetParameters() == nil {
+		c.SetParameters(parameters.NewParameters())
+	}
+	// Create the Data structure if it is nil
+	if c.GetData() == nil {
+		c.SetData(data.NewData())
+	}
+
+	// In case of AndX, we need to add the parameters to the Parameters structure first
+	if c.IsAndX() {
+		if c.GetAndX() == nil {
+			c.SetAndX(andx.NewAndX())
+			c.GetAndX().AndXCommand = c.GetCommandCode()
+		}
+
+		for _, parameter := range c.GetAndX().GetParameters() {
+			c.GetParameters().AddWord(parameter)
+		}
+	}
+
+	// First marshal the data and then the parameters
+	// This is because some parameters are dependent on the data, for example the size of some fields within
+	// the data will be stored in the parameters
+	rawDataContent := []byte{}
+
+	// Marshalling data Dialects
+	marshalledDialects, err := c.Dialects.Marshal()
+	if err != nil {
+		return nil, err
+	}
+	rawDataContent = append(rawDataContent, marshalledDialects...)
+
+	// Then marshal the parameters
+	rawParametersContent := []byte{}
+
+	// Marshalling parameters
+	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
+	marshalledParameters, err := c.GetParameters().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledParameters...)
+
+	// Marshalling data
+	c.GetData().Add(rawDataContent)
+	marshalledData, err := c.GetData().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledData...)
+
+	return marshalledCommand, nil
+}
+
+// Unmarshal unmarshals a byte array into the command structure
+//
+// Parameters:
+// - data: The byte array to unmarshal
+//
+// Returns:
+// - The number of bytes unmarshalled
+func (c *NegotiateRequest) Unmarshal(data []byte) (int, error) {
+	offset := 0
+
+	// First unmarshal the two structures
+	bytesRead, err := c.GetParameters().Unmarshal(data)
+	if err != nil {
+		return 0, err
+	}
+	rawParametersContent := c.GetParameters().GetBytes()
+	_, err = c.GetData().Unmarshal(data[bytesRead:])
+	if err != nil {
+		return 0, err
+	}
+	rawDataContent := c.GetData().GetBytes()
+
+	// First unmarshal the parameters
+	offset = 0
+
+	// Unmarshalling parameter WordCount
+	if len(rawParametersContent) < offset+1 {
+		return offset, fmt.Errorf("data too short for WordCount")
+	}
+	c.WordCount = types.UCHAR(rawParametersContent[offset])
+	offset++
+
+	// Then unmarshal the data
+	offset = 0
+
+	// Unmarshalling data Dialects
+	if len(rawDataContent) < offset+1 {
+		return offset, fmt.Errorf("rawParametersContent too short for Dialects")
+	}
+	bytesRead, err = c.Dialects.Unmarshal(rawDataContent[offset:])
+	if err != nil {
+		return 0, err
+	}
+	offset += bytesRead
+
+	return offset, nil
+}
