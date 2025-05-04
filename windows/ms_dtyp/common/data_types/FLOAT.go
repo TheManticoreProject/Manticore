e54fa0d64698ec3369714d3a5b6b09c015ae1eb7
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:09 +0200

    Added windows/ms_dtyp/common/data_types/FLOAT.go

---
+++ windows/ms_dtyp/common/data_types/FLOAT.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A FLOAT is a base type that is specified the IEEE Format section of [C706].section 14.2.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/73ba97b0-bbe5-483d-a0a8-46298ebcb160
+type FLOAT = float32
