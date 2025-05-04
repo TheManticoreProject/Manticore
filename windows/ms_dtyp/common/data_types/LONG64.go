c7514b345137c7d478b82fd9d1548ce88baab7f5
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:28 +0200

    Added windows/ms_dtyp/common/data_types/LONG64.go

---
+++ windows/ms_dtyp/common/data_types/LONG64.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A LONG64 is a 64-bit signed integer.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/7dc8dc9c-9d54-417a-933e-173933f0c329
+type LONG64 = int64
