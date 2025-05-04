2d793074e217fa7699463e70eac85cb87ce429cd
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:01 +0200

    Added windows/ms_dtyp/common/data_types/DWORD64.go

---
+++ windows/ms_dtyp/common/data_types/DWORD64.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A DWORD64 is a 64-bit unsigned integer.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/3f3bd817-6fdd-4db9-b542-f800f876007d
+type DWORD64 = uint64
