b7451dd7e44df9c4c73762ed8df808baf310449e
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:13 +0200

    Added windows/ms_dtyp/common/data_types/VOID.go

---
+++ windows/ms_dtyp/common/data_types/VOID.go
@@ -0,0 +1,5 @@
+package data_types
+
+// VOID is an alias for void.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/c0b7741b-f577-4eed-aff3-2e909df10a4d
+type VOID = struct{}
