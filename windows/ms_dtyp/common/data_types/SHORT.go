6bd76b4021d0869fde2117600c4ba5948ebecf8f
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:23 +0200

    Added windows/ms_dtyp/common/data_types/SHORT.go

---
+++ windows/ms_dtyp/common/data_types/SHORT.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A SHORT is a 16-bit signed integer(range: -32768 through 32767 decimal). The first bit (Most Significant Bit (MSB)) is the signing bit.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/47b1e7d6-b5a1-48c3-986e-b5e5eb3f06d2
+type SHORT = int16
