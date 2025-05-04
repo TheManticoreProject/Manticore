39002e40559ade1b7273145163df3d4c7fd730bd
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:56 +0200

    Added windows/ms_dtyp/common/data_types/LONG_PTR.go

---
+++ windows/ms_dtyp/common/data_types/LONG_PTR.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A LONG_PTR is a long type used for pointer precision. It is used when casting a pointer to a long type to perform pointer arithmetic.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/bbcfb0af-8349-4e98-ad26-957e1363f714
+type LONG_PTR = uintptr
