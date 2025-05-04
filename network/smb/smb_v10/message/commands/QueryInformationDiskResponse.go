5616d0a76fcf3aad068f7ef17930d7680f2faef7
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:57:33 +0200

    Added network/smb/smb_v10/message/commands/QueryInformationDiskResponse.go

---
+++ network/smb/smb_v10/message/commands/QueryInformationDiskResponse.go
@@ -0,0 +1,226 @@
+package commands
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/data"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/parameters"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/types"
+)
+
+// QueryInformationDiskResponse
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/3d5291fd-33f5-4899-b3ad-949b0d4d7f93
+type QueryInformationDiskResponse struct {
+	command_interface.Command
+
+	// Parameters
+	WordCount types.UCHAR
+	TotalUnits types.USHORT
+	BlocksPerUnit types.USHORT
+	BlockSize types.USHORT
+	FreeUnits types.USHORT
+	Reserved types.USHORT
+
+	// Data
+	ByteCount types.USHORT
+
+}
+
+// NewQueryInformationDiskResponse creates a new QueryInformationDiskResponse structure
+//
+// Returns:
+// - A pointer to the new QueryInformationDiskResponse structure
+func NewQueryInformationDiskResponse() *QueryInformationDiskResponse {
+	c := &QueryInformationDiskResponse{
+		// Parameters
+		WordCount: types.UCHAR(0),
+		TotalUnits: types.USHORT(0),
+		BlocksPerUnit: types.USHORT(0),
+		BlockSize: types.USHORT(0),
+		FreeUnits: types.USHORT(0),
+		Reserved: types.USHORT(0),
+
+		// Data
+		ByteCount: types.USHORT(0),
+
+	}
+
+	c.Command.SetCommandCode(codes.SMB_COM_QUERY_INFORMATION_DISK)
+
+	return c
+}
+
+
+
+// Marshal marshals the QueryInformationDiskResponse structure into a byte array
+//
+// Returns:
+// - A byte array representing the QueryInformationDiskResponse structure
+// - An error if the marshaling fails
+func (c *QueryInformationDiskResponse) Marshal() ([]byte, error) {
+	marshalledCommand := []byte{}
+
+	// Create the Parameters structure if it is nil
+	if c.GetParameters() == nil {
+		c.SetParameters(parameters.NewParameters())
+	}
+	// Create the Data structure if it is nil
+	if c.GetData() == nil {
+		c.SetData(data.NewData())
+	}
+
+	// In case of AndX, we need to add the parameters to the Parameters structure first
+	if c.IsAndX() {
+		if c.GetAndX() == nil {
+			c.SetAndX(andx.NewAndX())
+			c.GetAndX().AndXCommand = c.GetCommandCode()
+		}
+
+		for _, parameter := range c.GetAndX().GetParameters() {
+			c.GetParameters().AddWord(parameter)
+		}
+	}
+
+	// First marshal the data and then the parameters
+	// This is because some parameters are dependent on the data, for example the size of some fields within
+	// the data will be stored in the parameters
+	rawDataContent := []byte{}
+	
+	// Marshalling data ByteCount
+	buf2 := make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.ByteCount))
+	rawDataContent = append(rawDataContent, buf2...)
+	
+	// Then marshal the parameters
+	rawParametersContent := []byte{}
+	
+	// Marshalling parameter WordCount
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
+	
+	// Marshalling parameter TotalUnits
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.TotalUnits))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter BlocksPerUnit
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.BlocksPerUnit))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter BlockSize
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.BlockSize))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter FreeUnits
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.FreeUnits))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter Reserved
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.Reserved))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameters
+	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
+	marshalledParameters, err := c.GetParameters().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledParameters...)
+	
+	// Marshalling data
+	c.GetData().Add(rawDataContent)
+	marshalledData, err := c.GetData().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledData...)
+
+	return marshalledCommand, nil
+}
+
+// Unmarshal unmarshals a byte array into the command structure
+//
+// Parameters:
+// - data: The byte array to unmarshal
+//
+// Returns:
+// - The number of bytes unmarshalled
+func (c *QueryInformationDiskResponse) Unmarshal(data []byte) (int, error) {
+	offset := 0
+
+	// First unmarshal the two structures
+	bytesRead, err := c.GetParameters().Unmarshal(data)
+	if err != nil {
+		return 0, err
+	}
+	rawParametersContent := c.GetParameters().GetBytes()
+	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	if err != nil {
+		return 0, err
+	}
+	rawDataContent := c.GetData().GetBytes()
+
+	// First unmarshal the parameters
+	offset = 0
+	
+	// Unmarshalling parameter WordCount
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for WordCount")
+	}
+	c.WordCount = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter TotalUnits
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for TotalUnits")
+	}
+	c.TotalUnits = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter BlocksPerUnit
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for BlocksPerUnit")
+	}
+	c.BlocksPerUnit = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter BlockSize
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for BlockSize")
+	}
+	c.BlockSize = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter FreeUnits
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for FreeUnits")
+	}
+	c.FreeUnits = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter Reserved
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for Reserved")
+	}
+	c.Reserved = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Then unmarshal the data
+	offset = 0
+	
+	// Unmarshalling data ByteCount
+	if len(rawDataContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for ByteCount")
+	}
+	c.ByteCount = types.USHORT(binary.BigEndian.Uint16(rawDataContent[offset:offset+2]))
+	offset += 2
+
+	return offset, nil
+}
