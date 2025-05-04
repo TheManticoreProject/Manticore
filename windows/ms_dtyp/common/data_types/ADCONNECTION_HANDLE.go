3d4f2a3767d138ef765794abbc3038a37b5df614
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:11 +0200

    Added windows/ms_dtyp/common/data_types/ADCONNECTION_HANDLE.go

---
+++ windows/ms_dtyp/common/data_types/ADCONNECTION_HANDLE.go
@@ -0,0 +1,5 @@
+package data_types
+
+// ADCONNECTION_HANDLE
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/5faf669e-34a9-471b-aaff-8591b9650189
+type ADCONNECTION_HANDLE = uintptr
