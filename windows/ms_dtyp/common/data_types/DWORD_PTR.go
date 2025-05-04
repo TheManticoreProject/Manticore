fd7e8fce9b4f899c7062bd7ee48c2bb81dcc051b
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:29 +0200

    Added windows/ms_dtyp/common/data_types/DWORD_PTR.go

---
+++ windows/ms_dtyp/common/data_types/DWORD_PTR.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A DWORD_PTR is a pointer to a DWORD.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/66d61dd8-2191-4a37-b963-e49bf0dc2579
+type DWORD_PTR = *DWORD
