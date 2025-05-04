a50f6a6432eef478912f4b6a26ff590db70a4757
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:33 +0200

    Added windows/ms_dtyp/common/data_types/SIZE_T.go

---
+++ windows/ms_dtyp/common/data_types/SIZE_T.go
@@ -0,0 +1,5 @@
+package data_types
+
+// SIZE_T is a ULONG_PTR representing the maximum number of bytes to which a pointer can point.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/1dc2ff19-6fef-4c5f-b4fd-afbc2557fd81
+type SIZE_T = ULONG_PTR
