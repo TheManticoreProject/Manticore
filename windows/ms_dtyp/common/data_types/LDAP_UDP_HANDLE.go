e8e1bc01824edb69b47ce5e773a57441f557c414
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:02 +0200

    Added windows/ms_dtyp/common/data_types/LDAP_UDP_HANDLE.go

---
+++ windows/ms_dtyp/common/data_types/LDAP_UDP_HANDLE.go
@@ -0,0 +1,5 @@
+package data_types
+
+// LDAP_UDP_HANDLE
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/b0e4f011-fea4-473f-a678-be5e5a33f4cf
+type LDAP_UDP_HANDLE = uintptr
