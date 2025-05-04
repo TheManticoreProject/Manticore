c796c8e655ba503e2f0a4b1194813ecc9e8feb09
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:19 +0200

    Added windows/ms_dtyp/common/data_types/LPCVOID.go

---
+++ windows/ms_dtyp/common/data_types/LPCVOID.go
@@ -0,0 +1,5 @@
+package data_types
+
+// An LPCVOID is a 32-bit pointer to a constant of any type.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/66996877-9dd4-477d-a811-30e6c1a5525d
+type LPCVOID = uintptr
