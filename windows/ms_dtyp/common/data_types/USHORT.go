489c5ee338dcf91aeff1bafc9d1b8aaf3048e4f2
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:12 +0200

    Added windows/ms_dtyp/common/data_types/USHORT.go

---
+++ windows/ms_dtyp/common/data_types/USHORT.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A USHORT is a 16-bit unsigned integer (range: 0 through 65535 decimal). Because a USHORT is unsigned, its first bit (Most Significant Bit (MSB)) is not reserved for signing.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/c0618c5b-362b-4e06-9cb0-8720d240cf12
+type USHORT = uint16
