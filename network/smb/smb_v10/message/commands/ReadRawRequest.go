cf535b9120663fb6895e158d8d15e8896e33b532
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:56:42 +0200

    Added network/smb/smb_v10/message/commands/ReadRawRequest.go

---
+++ network/smb/smb_v10/message/commands/ReadRawRequest.go
@@ -0,0 +1,240 @@
+package commands
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/data"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/parameters"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/types"
+)
+
+// ReadRawRequest
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/1458b62a-18ed-4fb2-b8a9-ceabffb2c3b7
+type ReadRawRequest struct {
+	command_interface.Command
+
+	// Parameters
+	WordCount types.UCHAR
+	FID types.USHORT
+	Offset types.ULONG
+	MaxCountOfBytesToReturn types.USHORT
+	MinCountOfBytesToReturn types.USHORT
+	Timeout types.ULONG
+	Reserved types.USHORT
+
+	// Data
+	ByteCount types.USHORT
+
+}
+
+// NewReadRawRequest creates a new ReadRawRequest structure
+//
+// Returns:
+// - A pointer to the new ReadRawRequest structure
+func NewReadRawRequest() *ReadRawRequest {
+	c := &ReadRawRequest{
+		// Parameters
+		WordCount: types.UCHAR(0),
+		FID: types.USHORT(0),
+		Offset: types.ULONG(0),
+		MaxCountOfBytesToReturn: types.USHORT(0),
+		MinCountOfBytesToReturn: types.USHORT(0),
+		Timeout: types.ULONG(0),
+		Reserved: types.USHORT(0),
+
+		// Data
+		ByteCount: types.USHORT(0),
+
+	}
+
+	c.Command.SetCommandCode(codes.SMB_COM_READ_RAW)
+
+	return c
+}
+
+
+
+// Marshal marshals the ReadRawRequest structure into a byte array
+//
+// Returns:
+// - A byte array representing the ReadRawRequest structure
+// - An error if the marshaling fails
+func (c *ReadRawRequest) Marshal() ([]byte, error) {
+	marshalledCommand := []byte{}
+
+	// Create the Parameters structure if it is nil
+	if c.GetParameters() == nil {
+		c.SetParameters(parameters.NewParameters())
+	}
+	// Create the Data structure if it is nil
+	if c.GetData() == nil {
+		c.SetData(data.NewData())
+	}
+
+	// In case of AndX, we need to add the parameters to the Parameters structure first
+	if c.IsAndX() {
+		if c.GetAndX() == nil {
+			c.SetAndX(andx.NewAndX())
+			c.GetAndX().AndXCommand = c.GetCommandCode()
+		}
+
+		for _, parameter := range c.GetAndX().GetParameters() {
+			c.GetParameters().AddWord(parameter)
+		}
+	}
+
+	// First marshal the data and then the parameters
+	// This is because some parameters are dependent on the data, for example the size of some fields within
+	// the data will be stored in the parameters
+	rawDataContent := []byte{}
+	
+	// Marshalling data ByteCount
+	buf2 := make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.ByteCount))
+	rawDataContent = append(rawDataContent, buf2...)
+	
+	// Then marshal the parameters
+	rawParametersContent := []byte{}
+	
+	// Marshalling parameter WordCount
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
+	
+	// Marshalling parameter FID
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.FID))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter Offset
+	buf4 := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf4, uint32(c.Offset))
+	rawParametersContent = append(rawParametersContent, buf4...)
+	
+	// Marshalling parameter MaxCountOfBytesToReturn
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.MaxCountOfBytesToReturn))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter MinCountOfBytesToReturn
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.MinCountOfBytesToReturn))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter Timeout
+	buf4 = make([]byte, 4)
+	binary.BigEndian.PutUint32(buf4, uint32(c.Timeout))
+	rawParametersContent = append(rawParametersContent, buf4...)
+	
+	// Marshalling parameter Reserved
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.Reserved))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameters
+	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
+	marshalledParameters, err := c.GetParameters().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledParameters...)
+	
+	// Marshalling data
+	c.GetData().Add(rawDataContent)
+	marshalledData, err := c.GetData().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledData...)
+
+	return marshalledCommand, nil
+}
+
+// Unmarshal unmarshals a byte array into the command structure
+//
+// Parameters:
+// - data: The byte array to unmarshal
+//
+// Returns:
+// - The number of bytes unmarshalled
+func (c *ReadRawRequest) Unmarshal(data []byte) (int, error) {
+	offset := 0
+
+	// First unmarshal the two structures
+	bytesRead, err := c.GetParameters().Unmarshal(data)
+	if err != nil {
+		return 0, err
+	}
+	rawParametersContent := c.GetParameters().GetBytes()
+	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	if err != nil {
+		return 0, err
+	}
+	rawDataContent := c.GetData().GetBytes()
+
+	// First unmarshal the parameters
+	offset = 0
+	
+	// Unmarshalling parameter WordCount
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for WordCount")
+	}
+	c.WordCount = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter FID
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for FID")
+	}
+	c.FID = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter Offset
+	if len(rawParametersContent) < offset+4 {
+	    return offset, fmt.Errorf("rawParametersContent too short for Offset")
+	}
+	c.Offset = types.ULONG(binary.BigEndian.Uint32(rawParametersContent[offset:offset+4]))
+	offset += 4
+	
+	// Unmarshalling parameter MaxCountOfBytesToReturn
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for MaxCountOfBytesToReturn")
+	}
+	c.MaxCountOfBytesToReturn = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter MinCountOfBytesToReturn
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for MinCountOfBytesToReturn")
+	}
+	c.MinCountOfBytesToReturn = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter Timeout
+	if len(rawParametersContent) < offset+4 {
+	    return offset, fmt.Errorf("rawParametersContent too short for Timeout")
+	}
+	c.Timeout = types.ULONG(binary.BigEndian.Uint32(rawParametersContent[offset:offset+4]))
+	offset += 4
+	
+	// Unmarshalling parameter Reserved
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for Reserved")
+	}
+	c.Reserved = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Then unmarshal the data
+	offset = 0
+	
+	// Unmarshalling data ByteCount
+	if len(rawDataContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for ByteCount")
+	}
+	c.ByteCount = types.USHORT(binary.BigEndian.Uint16(rawDataContent[offset:offset+2]))
+	offset += 2
+
+	return offset, nil
+}
