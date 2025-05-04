e98f05eadeb22d7927951b082d5db7d12fe77dd5
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:25 +0200

    Added windows/ms_dtyp/common/data_types/ERROR_STATUS_T.go

---
+++ windows/ms_dtyp/common/data_types/ERROR_STATUS_T.go
@@ -0,0 +1,5 @@
+package data_types
+
+// ERROR_STATUS_T
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/802ada7f-c5fc-415e-8438-c7369bb4193b
+type ERROR_STATUS_T = ULONG
