c4a8b79bad4a955b8d39f1e076206d672ba31725
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:56:56 +0200

    Added network/smb/smb_v10/message/commands/LockingAndxRequest.go

---
+++ network/smb/smb_v10/message/commands/LockingAndxRequest.go
@@ -0,0 +1,266 @@
+package commands
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/data"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/parameters"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/types"
+)
+
+// LockingAndxRequest
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/b5c6eae7-976b-4444-b52e-c76c68c861ad
+type LockingAndxRequest struct {
+	command_interface.Command
+
+	// Parameters
+	WordCount types.UCHAR
+	AndXCommand types.UCHAR
+	AndXReserved types.UCHAR
+	AndXOffset types.USHORT
+	FID types.USHORT
+	TypeOfLock types.UCHAR
+	NewOpLockLevel types.UCHAR
+	Timeout types.ULONG
+	NumberOfRequestedUnlocks types.USHORT
+	NumberOfRequestedLocks types.USHORT
+
+	// Data
+
+}
+
+// NewLockingAndxRequest creates a new LockingAndxRequest structure
+//
+// Returns:
+// - A pointer to the new LockingAndxRequest structure
+func NewLockingAndxRequest() *LockingAndxRequest {
+	c := &LockingAndxRequest{
+		// Parameters
+		WordCount: types.UCHAR(0),
+		AndXCommand: types.UCHAR(0),
+		AndXReserved: types.UCHAR(0),
+		AndXOffset: types.USHORT(0),
+		FID: types.USHORT(0),
+		TypeOfLock: types.UCHAR(0),
+		NewOpLockLevel: types.UCHAR(0),
+		Timeout: types.ULONG(0),
+		NumberOfRequestedUnlocks: types.USHORT(0),
+		NumberOfRequestedLocks: types.USHORT(0),
+
+		// Data
+
+	}
+
+	c.Command.SetCommandCode(codes.SMB_COM_LOCKING_ANDX)
+
+	return c
+}
+
+
+// IsAndX returns true if the command is an AndX
+func (c *LockingAndxRequest) IsAndX() bool {
+	return true
+}
+
+
+
+// Marshal marshals the LockingAndxRequest structure into a byte array
+//
+// Returns:
+// - A byte array representing the LockingAndxRequest structure
+// - An error if the marshaling fails
+func (c *LockingAndxRequest) Marshal() ([]byte, error) {
+	marshalledCommand := []byte{}
+
+	// Create the Parameters structure if it is nil
+	if c.GetParameters() == nil {
+		c.SetParameters(parameters.NewParameters())
+	}
+	// Create the Data structure if it is nil
+	if c.GetData() == nil {
+		c.SetData(data.NewData())
+	}
+
+	// In case of AndX, we need to add the parameters to the Parameters structure first
+	if c.IsAndX() {
+		if c.GetAndX() == nil {
+			c.SetAndX(andx.NewAndX())
+			c.GetAndX().AndXCommand = c.GetCommandCode()
+		}
+
+		for _, parameter := range c.GetAndX().GetParameters() {
+			c.GetParameters().AddWord(parameter)
+		}
+	}
+
+	// First marshal the data and then the parameters
+	// This is because some parameters are dependent on the data, for example the size of some fields within
+	// the data will be stored in the parameters
+	rawDataContent := []byte{}
+	
+	// Then marshal the parameters
+	rawParametersContent := []byte{}
+	
+	// Marshalling parameter WordCount
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
+	
+	// Marshalling parameter AndXCommand
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.AndXCommand))
+	
+	// Marshalling parameter AndXReserved
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.AndXReserved))
+	
+	// Marshalling parameter AndXOffset
+	buf2 := make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.AndXOffset))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter FID
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.FID))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter TypeOfLock
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.TypeOfLock))
+	
+	// Marshalling parameter NewOpLockLevel
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.NewOpLockLevel))
+	
+	// Marshalling parameter Timeout
+	buf4 := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf4, uint32(c.Timeout))
+	rawParametersContent = append(rawParametersContent, buf4...)
+	
+	// Marshalling parameter NumberOfRequestedUnlocks
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.NumberOfRequestedUnlocks))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter NumberOfRequestedLocks
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.NumberOfRequestedLocks))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameters
+	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
+	marshalledParameters, err := c.GetParameters().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledParameters...)
+	
+	// Marshalling data
+	c.GetData().Add(rawDataContent)
+	marshalledData, err := c.GetData().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledData...)
+
+	return marshalledCommand, nil
+}
+
+// Unmarshal unmarshals a byte array into the command structure
+//
+// Parameters:
+// - data: The byte array to unmarshal
+//
+// Returns:
+// - The number of bytes unmarshalled
+func (c *LockingAndxRequest) Unmarshal(data []byte) (int, error) {
+	offset := 0
+
+	// First unmarshal the two structures
+	bytesRead, err := c.GetParameters().Unmarshal(data)
+	if err != nil {
+		return 0, err
+	}
+	rawParametersContent := c.GetParameters().GetBytes()
+	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	if err != nil {
+		return 0, err
+	}
+	rawDataContent := c.GetData().GetBytes()
+
+	// First unmarshal the parameters
+	offset = 0
+	
+	// Unmarshalling parameter WordCount
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for WordCount")
+	}
+	c.WordCount = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter AndXCommand
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for AndXCommand")
+	}
+	c.AndXCommand = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter AndXReserved
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for AndXReserved")
+	}
+	c.AndXReserved = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter AndXOffset
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for AndXOffset")
+	}
+	c.AndXOffset = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter FID
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for FID")
+	}
+	c.FID = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter TypeOfLock
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for TypeOfLock")
+	}
+	c.TypeOfLock = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter NewOpLockLevel
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for NewOpLockLevel")
+	}
+	c.NewOpLockLevel = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter Timeout
+	if len(rawParametersContent) < offset+4 {
+	    return offset, fmt.Errorf("rawParametersContent too short for Timeout")
+	}
+	c.Timeout = types.ULONG(binary.BigEndian.Uint32(rawParametersContent[offset:offset+4]))
+	offset += 4
+	
+	// Unmarshalling parameter NumberOfRequestedUnlocks
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for NumberOfRequestedUnlocks")
+	}
+	c.NumberOfRequestedUnlocks = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter NumberOfRequestedLocks
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for NumberOfRequestedLocks")
+	}
+	c.NumberOfRequestedLocks = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Then unmarshal the data
+	offset = 0
+
+	return offset, nil
+}
