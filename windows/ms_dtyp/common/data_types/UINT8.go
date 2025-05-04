b1cf86cb7aa07fd9e1b253ff0c46859529fb2a44
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:20 +0200

    Added windows/ms_dtyp/common/data_types/UINT8.go

---
+++ windows/ms_dtyp/common/data_types/UINT8.go
@@ -0,0 +1,6 @@
+package data_types
+
+// A UINT8 is an 8-bit unsigned integer (range: 0 through 255 decimal).
+// Because a UINT8 is unsigned, its first bit (Most Significant Bit (MSB)) is not reserved for signing.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/a88ed362-a905-4ed2-85f5-cfc8692c9842
+type UINT8 = uint8
