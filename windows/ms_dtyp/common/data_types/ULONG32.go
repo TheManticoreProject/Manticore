45a5f901cff2c97c629c09768eced1e3513d49c8
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:41 +0200

    Added windows/ms_dtyp/common/data_types/ULONG32.go

---
+++ windows/ms_dtyp/common/data_types/ULONG32.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A ULONG32 is an unsigned LONG32.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/c9346e52-0765-4247-ab74-89c24ad3517b
+type ULONG32 = uint32
