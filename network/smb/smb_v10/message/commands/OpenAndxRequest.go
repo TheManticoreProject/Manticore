f9233285261f7318966432943b3fdd4eab935ab0
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:56:50 +0200

    Added network/smb/smb_v10/message/commands/OpenAndxRequest.go

---
+++ network/smb/smb_v10/message/commands/OpenAndxRequest.go
@@ -0,0 +1,303 @@
+package commands
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/data"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/parameters"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/types"
+)
+
+// OpenAndxRequest
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/3a760987-f60d-4012-930b-fe90328775cc
+type OpenAndxRequest struct {
+	command_interface.Command
+
+	// Parameters
+	WordCount types.UCHAR
+	AndXCommand types.UCHAR
+	AndXReserved types.UCHAR
+	AndXOffset types.USHORT
+	Flags types.USHORT
+	AccessMode types.USHORT
+	SearchAttrs types.SMB_FILE_ATTRIBUTES
+	FileAttrs types.SMB_FILE_ATTRIBUTES
+	CreationTime types.FILETIME
+	OpenMode types.USHORT
+	AllocationSize types.ULONG
+	Timeout types.ULONG
+
+	// Data
+	FileName types.SMB_STRING
+
+}
+
+// NewOpenAndxRequest creates a new OpenAndxRequest structure
+//
+// Returns:
+// - A pointer to the new OpenAndxRequest structure
+func NewOpenAndxRequest() *OpenAndxRequest {
+	c := &OpenAndxRequest{
+		// Parameters
+		WordCount: types.UCHAR(0),
+		AndXCommand: types.UCHAR(0),
+		AndXReserved: types.UCHAR(0),
+		AndXOffset: types.USHORT(0),
+		Flags: types.USHORT(0),
+		AccessMode: types.USHORT(0),
+		SearchAttrs: types.SMB_FILE_ATTRIBUTES{},
+		FileAttrs: types.SMB_FILE_ATTRIBUTES{},
+		CreationTime: types.FILETIME{},
+		OpenMode: types.USHORT(0),
+		AllocationSize: types.ULONG(0),
+		Timeout: types.ULONG(0),
+
+		// Data
+		FileName: types.SMB_STRING{},
+
+	}
+
+	c.Command.SetCommandCode(codes.SMB_COM_OPEN_ANDX)
+
+	return c
+}
+
+
+// IsAndX returns true if the command is an AndX
+func (c *OpenAndxRequest) IsAndX() bool {
+	return true
+}
+
+
+
+// Marshal marshals the OpenAndxRequest structure into a byte array
+//
+// Returns:
+// - A byte array representing the OpenAndxRequest structure
+// - An error if the marshaling fails
+func (c *OpenAndxRequest) Marshal() ([]byte, error) {
+	marshalledCommand := []byte{}
+
+	// Create the Parameters structure if it is nil
+	if c.GetParameters() == nil {
+		c.SetParameters(parameters.NewParameters())
+	}
+	// Create the Data structure if it is nil
+	if c.GetData() == nil {
+		c.SetData(data.NewData())
+	}
+
+	// In case of AndX, we need to add the parameters to the Parameters structure first
+	if c.IsAndX() {
+		if c.GetAndX() == nil {
+			c.SetAndX(andx.NewAndX())
+			c.GetAndX().AndXCommand = c.GetCommandCode()
+		}
+
+		for _, parameter := range c.GetAndX().GetParameters() {
+			c.GetParameters().AddWord(parameter)
+		}
+	}
+
+	// First marshal the data and then the parameters
+	// This is because some parameters are dependent on the data, for example the size of some fields within
+	// the data will be stored in the parameters
+	rawDataContent := []byte{}
+	
+	// Marshalling data FileName
+	bytesStream, err := c.FileName.Marshal()
+	if err != nil {
+			return nil, err
+	}
+	rawDataContent = append(rawDataContent, bytesStream...)
+	
+	// Then marshal the parameters
+	rawParametersContent := []byte{}
+	
+	// Marshalling parameter WordCount
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
+	
+	// Marshalling parameter AndXCommand
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.AndXCommand))
+	
+	// Marshalling parameter AndXReserved
+	rawParametersContent = append(rawParametersContent, types.UCHAR(c.AndXReserved))
+	
+	// Marshalling parameter AndXOffset
+	buf2 := make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.AndXOffset))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter Flags
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.Flags))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter AccessMode
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.AccessMode))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter SearchAttrs
+	
+	// Marshalling parameter FileAttrs
+	
+	// Marshalling parameter CreationTime
+	bytesStream, err := c.CreationTime.Marshal()
+	if err != nil {
+			return nil, err
+	}
+	rawParametersContent = append(rawParametersContent, bytesStream...)
+	
+	// Marshalling parameter OpenMode
+	buf2 = make([]byte, 2)
+	binary.BigEndian.PutUint16(buf2, uint16(c.OpenMode))
+	rawParametersContent = append(rawParametersContent, buf2...)
+	
+	// Marshalling parameter AllocationSize
+	buf4 := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf4, uint32(c.AllocationSize))
+	rawParametersContent = append(rawParametersContent, buf4...)
+	
+	// Marshalling parameter Timeout
+	buf4 = make([]byte, 4)
+	binary.BigEndian.PutUint32(buf4, uint32(c.Timeout))
+	rawParametersContent = append(rawParametersContent, buf4...)
+	
+	// Marshalling parameters
+	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
+	marshalledParameters, err := c.GetParameters().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledParameters...)
+	
+	// Marshalling data
+	c.GetData().Add(rawDataContent)
+	marshalledData, err := c.GetData().Marshal()
+	if err != nil {
+		return nil, err
+	}
+	marshalledCommand = append(marshalledCommand, marshalledData...)
+
+	return marshalledCommand, nil
+}
+
+// Unmarshal unmarshals a byte array into the command structure
+//
+// Parameters:
+// - data: The byte array to unmarshal
+//
+// Returns:
+// - The number of bytes unmarshalled
+func (c *OpenAndxRequest) Unmarshal(data []byte) (int, error) {
+	offset := 0
+
+	// First unmarshal the two structures
+	bytesRead, err := c.GetParameters().Unmarshal(data)
+	if err != nil {
+		return 0, err
+	}
+	rawParametersContent := c.GetParameters().GetBytes()
+	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	if err != nil {
+		return 0, err
+	}
+	rawDataContent := c.GetData().GetBytes()
+
+	// First unmarshal the parameters
+	offset = 0
+	
+	// Unmarshalling parameter WordCount
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for WordCount")
+	}
+	c.WordCount = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter AndXCommand
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for AndXCommand")
+	}
+	c.AndXCommand = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter AndXReserved
+	if len(rawParametersContent) < offset+1 {
+	    return offset, fmt.Errorf("data too short for AndXReserved")
+	}
+	c.AndXReserved = types.UCHAR(rawParametersContent[offset])
+	offset++
+	
+	// Unmarshalling parameter AndXOffset
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for AndXOffset")
+	}
+	c.AndXOffset = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter Flags
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for Flags")
+	}
+	c.Flags = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter AccessMode
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for AccessMode")
+	}
+	c.AccessMode = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter SearchAttrs
+	
+	// Unmarshalling parameter FileAttrs
+	
+	// Unmarshalling parameter CreationTime
+	if len(rawParametersContent) < offset+8 {
+	    return offset, fmt.Errorf("rawParametersContent too short for CreationTime")
+	}
+	bytesRead, err := c.CreationTime.Unmarshal(rawParametersContent[offset:])
+	if err != nil {
+	    return offset, err
+	}
+	offset += bytesRead
+	
+	// Unmarshalling parameter OpenMode
+	if len(rawParametersContent) < offset+2 {
+	    return offset, fmt.Errorf("rawParametersContent too short for OpenMode")
+	}
+	c.OpenMode = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	offset += 2
+	
+	// Unmarshalling parameter AllocationSize
+	if len(rawParametersContent) < offset+4 {
+	    return offset, fmt.Errorf("rawParametersContent too short for AllocationSize")
+	}
+	c.AllocationSize = types.ULONG(binary.BigEndian.Uint32(rawParametersContent[offset:offset+4]))
+	offset += 4
+	
+	// Unmarshalling parameter Timeout
+	if len(rawParametersContent) < offset+4 {
+	    return offset, fmt.Errorf("rawParametersContent too short for Timeout")
+	}
+	c.Timeout = types.ULONG(binary.BigEndian.Uint32(rawParametersContent[offset:offset+4]))
+	offset += 4
+	
+	// Then unmarshal the data
+	offset = 0
+	
+	// Unmarshalling data FileName
+	bytesRead, err := c.FileName.Unmarshal(rawDataContent[offset:])
+	if err != nil {
+	    return offset, err
+	}
+	offset += bytesRead
+
+	return offset, nil
+}
