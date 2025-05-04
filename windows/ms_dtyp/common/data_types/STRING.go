897f30dc35e3fdb6b5b29eea68d37ba16b481fd9
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:55:05 +0200

    Added windows/ms_dtyp/common/data_types/STRING.go

---
+++ windows/ms_dtyp/common/data_types/STRING.go
@@ -0,0 +1,5 @@
+package data_types
+
+// Unless otherwise noted, a STRING is a UCHAR buffer that represents a null-terminated string of 8-bit characters.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/08460486-bacc-48af-8520-195ff7f80db5
+type STRING = []UCHAR
