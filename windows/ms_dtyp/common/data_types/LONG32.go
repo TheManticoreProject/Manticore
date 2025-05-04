f130391ee1737208a1dbf489b57ddc858f9377f7
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:45 +0200

    Added windows/ms_dtyp/common/data_types/LONG32.go

---
+++ windows/ms_dtyp/common/data_types/LONG32.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A LONG32 is a 32-bit signed integer.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/286e80a4-16fa-45d5-b97d-aeac136ef3f0
+type LONG32 = int32
