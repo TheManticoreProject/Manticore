7f054a27147703c2f9f5f7bb455a917c5fca0f2b
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:39 +0200

    Added windows/ms_dtyp/common/data_types/UNICODE.go

---
+++ windows/ms_dtyp/common/data_types/UNICODE.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A single Unicode character.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/845b6fa4-c34a-4b90-824d-60e98533dfb5
+type UNICODE = uint16
