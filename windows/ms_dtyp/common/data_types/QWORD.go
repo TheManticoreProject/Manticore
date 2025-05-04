20fa6dd64caccd50ceea8d9f9ff1d8061335e1b9
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:21 +0200

    Added windows/ms_dtyp/common/data_types/QWORD.go

---
+++ windows/ms_dtyp/common/data_types/QWORD.go
@@ -0,0 +1,5 @@
+package data_types
+
+// A QWORD is a 64-bit unsigned integer.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/ac050bbf-a821-4fab-bccf-d95d892f428f
+type QWORD = uint64
