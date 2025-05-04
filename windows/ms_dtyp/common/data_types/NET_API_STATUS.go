9cfae3af288adca9c4cb7ebbacf01761e0de0a41
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-04 20:54:43 +0200

    Added windows/ms_dtyp/common/data_types/NET_API_STATUS.go

---
+++ windows/ms_dtyp/common/data_types/NET_API_STATUS.go
@@ -0,0 +1,5 @@
+package data_types
+
+// NET_API_STATUS
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/19cdb498-c67b-42b7-a540-4bc6a73c04f6
+type NET_API_STATUS = DWORD
