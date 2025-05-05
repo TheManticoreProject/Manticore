5179883f1a06438c7caaab125bce63407d2855d2
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-05 14:27:25 +0200

    Fixed FindClose2Request

--- network/smb/smb_v10/message/commands/FindClose2Request.go
+++ network/smb/smb_v10/message/commands/FindClose2Request.go
@@ -18,12 +18,9 @@ type FindClose2Request struct {
 	command_interface.Command
 
 	// Parameters
-	WordCount types.UCHAR
+
+	// A search handle, also known as a Search ID (SID). This MUST be the SID value returned in the initial TRANS2_FIND_FIRST2 subcommand request.
 	SearchHandle types.USHORT
-
-	// Data
-	ByteCount types.USHORT
-
 }
 
 // NewFindClose2Request creates a new FindClose2Request structure
@@ -33,20 +30,13 @@ type FindClose2Request struct {
 func NewFindClose2Request() *FindClose2Request {
 	c := &FindClose2Request{
 		// Parameters
-		WordCount: types.UCHAR(0),
 		SearchHandle: types.USHORT(0),
-
-		// Data
-		ByteCount: types.USHORT(0),
-
 	}
 
 	c.Command.SetCommandCode(codes.SMB_COM_FIND_CLOSE2)
 
 	return c
 }
-
-
 
 // Marshal marshals the FindClose2Request structure into a byte array
 //
@@ -81,23 +71,15 @@ func (c *FindClose2Request) Marshal() ([]byte, error) {
 	// This is because some parameters are dependent on the data, for example the size of some fields within
 	// the data will be stored in the parameters
 	rawDataContent := []byte{}
-	
-	// Marshalling data ByteCount
-	buf2 := make([]byte, 2)
-	binary.BigEndian.PutUint16(buf2, uint16(c.ByteCount))
-	rawDataContent = append(rawDataContent, buf2...)
-	
+
 	// Then marshal the parameters
 	rawParametersContent := []byte{}
-	
-	// Marshalling parameter WordCount
-	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
-	
+
 	// Marshalling parameter SearchHandle
-	buf2 = make([]byte, 2)
+	buf2 := make([]byte, 2)
 	binary.BigEndian.PutUint16(buf2, uint16(c.SearchHandle))
 	rawParametersContent = append(rawParametersContent, buf2...)
-	
+
 	// Marshalling parameters
 	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
 	marshalledParameters, err := c.GetParameters().Marshal()
@@ -105,7 +87,7 @@ func (c *FindClose2Request) Marshal() ([]byte, error) {
 		return nil, err
 	}
 	marshalledCommand = append(marshalledCommand, marshalledParameters...)
-	
+
 	// Marshalling data
 	c.GetData().Add(rawDataContent)
 	marshalledData, err := c.GetData().Marshal()
@@ -137,34 +119,21 @@ func (c *FindClose2Request) Unmarshal(data []byte) (int, error) {
 	if err != nil {
 		return 0, err
 	}
-	rawDataContent := c.GetData().GetBytes()
+	_ = c.GetData().GetBytes()
 
 	// First unmarshal the parameters
 	offset = 0
-	
-	// Unmarshalling parameter WordCount
-	if len(rawParametersContent) < offset+1 {
-	    return offset, fmt.Errorf("data too short for WordCount")
-	}
-	c.WordCount = types.UCHAR(rawParametersContent[offset])
-	offset++
-	
+
 	// Unmarshalling parameter SearchHandle
 	if len(rawParametersContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for SearchHandle")
+		return offset, fmt.Errorf("rawParametersContent too short for SearchHandle")
 	}
-	c.SearchHandle = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	c.SearchHandle = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset : offset+2]))
 	offset += 2
-	
+
 	// Then unmarshal the data
 	offset = 0
-	
-	// Unmarshalling data ByteCount
-	if len(rawDataContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for ByteCount")
-	}
-	c.ByteCount = types.USHORT(binary.BigEndian.Uint16(rawDataContent[offset:offset+2]))
-	offset += 2
+	// No data is sent by this message.
 
 	return offset, nil
 }
