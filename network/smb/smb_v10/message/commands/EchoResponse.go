96480fbe97b4735cd01055c41a31b3bbd86a39f1
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-05 11:20:26 +0200

    Fixed EchoResponse

--- network/smb/smb_v10/message/commands/EchoResponse.go
+++ network/smb/smb_v10/message/commands/EchoResponse.go
@@ -18,11 +18,7 @@ type EchoResponse struct {
 	command_interface.Command
 
 	// Parameters
-	WordCount types.UCHAR
 	SequenceNumber types.USHORT
-
-	// Data
-
 }
 
 // NewEchoResponse creates a new EchoResponse structure
@@ -32,19 +28,13 @@ type EchoResponse struct {
 func NewEchoResponse() *EchoResponse {
 	c := &EchoResponse{
 		// Parameters
-		WordCount: types.UCHAR(0),
 		SequenceNumber: types.USHORT(0),
-
-		// Data
-
 	}
 
 	c.Command.SetCommandCode(codes.SMB_COM_ECHO)
 
 	return c
 }
-
-
 
 // Marshal marshals the EchoResponse structure into a byte array
 //
@@ -79,18 +69,15 @@ func (c *EchoResponse) Marshal() ([]byte, error) {
 	// This is because some parameters are dependent on the data, for example the size of some fields within
 	// the data will be stored in the parameters
 	rawDataContent := []byte{}
-	
+
 	// Then marshal the parameters
 	rawParametersContent := []byte{}
-	
-	// Marshalling parameter WordCount
-	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
-	
+
 	// Marshalling parameter SequenceNumber
 	buf2 := make([]byte, 2)
 	binary.BigEndian.PutUint16(buf2, uint16(c.SequenceNumber))
 	rawParametersContent = append(rawParametersContent, buf2...)
-	
+
 	// Marshalling parameters
 	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
 	marshalledParameters, err := c.GetParameters().Marshal()
@@ -98,7 +85,7 @@ func (c *EchoResponse) Marshal() ([]byte, error) {
 		return nil, err
 	}
 	marshalledCommand = append(marshalledCommand, marshalledParameters...)
-	
+
 	// Marshalling data
 	c.GetData().Add(rawDataContent)
 	marshalledData, err := c.GetData().Marshal()
@@ -130,25 +117,18 @@ func (c *EchoResponse) Unmarshal(data []byte) (int, error) {
 	if err != nil {
 		return 0, err
 	}
-	rawDataContent := c.GetData().GetBytes()
+	_ = c.GetData().GetBytes()
 
 	// First unmarshal the parameters
 	offset = 0
-	
-	// Unmarshalling parameter WordCount
-	if len(rawParametersContent) < offset+1 {
-	    return offset, fmt.Errorf("data too short for WordCount")
-	}
-	c.WordCount = types.UCHAR(rawParametersContent[offset])
-	offset++
-	
+
 	// Unmarshalling parameter SequenceNumber
 	if len(rawParametersContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for SequenceNumber")
+		return offset, fmt.Errorf("rawParametersContent too short for SequenceNumber")
 	}
-	c.SequenceNumber = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	c.SequenceNumber = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset : offset+2]))
 	offset += 2
-	
+
 	// Then unmarshal the data
 	offset = 0
 
