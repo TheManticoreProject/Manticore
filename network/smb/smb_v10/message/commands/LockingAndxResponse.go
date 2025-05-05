b285365945bf386092afa153cac67e7cb8a485ce
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-05 22:02:32 +0200

    Fixed LockingAndxResponse

--- network/smb/smb_v10/message/commands/LockingAndxResponse.go
+++ network/smb/smb_v10/message/commands/LockingAndxResponse.go
@@ -1,31 +1,17 @@
 package commands
 
 import (
-	"encoding/binary"
-	"fmt"
-
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/data"
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/parameters"
-	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/types"
 )
 
 // LockingAndxResponse
 // Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/165cd91f-1207-419e-bb62-eb305ea9a67a
 type LockingAndxResponse struct {
 	command_interface.Command
-
-	// Parameters
-	WordCount types.UCHAR
-	AndXCommand types.UCHAR
-	AndXReserved types.UCHAR
-	AndXOffset types.UCHAR
-
-	// Data
-	ByteCount types.USHORT
-
 }
 
 // NewLockingAndxResponse creates a new LockingAndxResponse structure
@@ -33,30 +19,17 @@ type LockingAndxResponse struct {
 // Returns:
 // - A pointer to the new LockingAndxResponse structure
 func NewLockingAndxResponse() *LockingAndxResponse {
-	c := &LockingAndxResponse{
-		// Parameters
-		WordCount: types.UCHAR(0),
-		AndXCommand: types.UCHAR(0),
-		AndXReserved: types.UCHAR(0),
-		AndXOffset: types.UCHAR(0),
-
-		// Data
-		ByteCount: types.USHORT(0),
-
-	}
+	c := &LockingAndxResponse{}
 
 	c.Command.SetCommandCode(codes.SMB_COM_LOCKING_ANDX)
 
 	return c
 }
 
-
 // IsAndX returns true if the command is an AndX
 func (c *LockingAndxResponse) IsAndX() bool {
 	return true
 }
-
-
 
 // Marshal marshals the LockingAndxResponse structure into a byte array
 //
@@ -91,27 +64,10 @@ func (c *LockingAndxResponse) Marshal() ([]byte, error) {
 	// This is because some parameters are dependent on the data, for example the size of some fields within
 	// the data will be stored in the parameters
 	rawDataContent := []byte{}
-	
-	// Marshalling data ByteCount
-	buf2 := make([]byte, 2)
-	binary.BigEndian.PutUint16(buf2, uint16(c.ByteCount))
-	rawDataContent = append(rawDataContent, buf2...)
-	
+
 	// Then marshal the parameters
 	rawParametersContent := []byte{}
-	
-	// Marshalling parameter WordCount
-	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
-	
-	// Marshalling parameter AndXCommand
-	rawParametersContent = append(rawParametersContent, types.UCHAR(c.AndXCommand))
-	
-	// Marshalling parameter AndXReserved
-	rawParametersContent = append(rawParametersContent, types.UCHAR(c.AndXReserved))
-	
-	// Marshalling parameter AndXOffset
-	rawParametersContent = append(rawParametersContent, types.UCHAR(c.AndXOffset))
-	
+
 	// Marshalling parameters
 	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
 	marshalledParameters, err := c.GetParameters().Marshal()
@@ -119,7 +75,7 @@ func (c *LockingAndxResponse) Marshal() ([]byte, error) {
 		return nil, err
 	}
 	marshalledCommand = append(marshalledCommand, marshalledParameters...)
-	
+
 	// Marshalling data
 	c.GetData().Add(rawDataContent)
 	marshalledData, err := c.GetData().Marshal()
@@ -146,53 +102,20 @@ func (c *LockingAndxResponse) Unmarshal(data []byte) (int, error) {
 	if err != nil {
 		return 0, err
 	}
-	rawParametersContent := c.GetParameters().GetBytes()
+	_ = c.GetParameters().GetBytes()
 	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
-	rawDataContent := c.GetData().GetBytes()
+	_ = c.GetData().GetBytes()
 
 	// First unmarshal the parameters
 	offset = 0
-	
-	// Unmarshalling parameter WordCount
-	if len(rawParametersContent) < offset+1 {
-	    return offset, fmt.Errorf("data too short for WordCount")
-	}
-	c.WordCount = types.UCHAR(rawParametersContent[offset])
-	offset++
-	
-	// Unmarshalling parameter AndXCommand
-	if len(rawParametersContent) < offset+1 {
-	    return offset, fmt.Errorf("data too short for AndXCommand")
-	}
-	c.AndXCommand = types.UCHAR(rawParametersContent[offset])
-	offset++
-	
-	// Unmarshalling parameter AndXReserved
-	if len(rawParametersContent) < offset+1 {
-	    return offset, fmt.Errorf("data too short for AndXReserved")
-	}
-	c.AndXReserved = types.UCHAR(rawParametersContent[offset])
-	offset++
-	
-	// Unmarshalling parameter AndXOffset
-	if len(rawParametersContent) < offset+1 {
-	    return offset, fmt.Errorf("data too short for AndXOffset")
-	}
-	c.AndXOffset = types.UCHAR(rawParametersContent[offset])
-	offset++
-	
+	// No parameters are sent by this message.
+
 	// Then unmarshal the data
 	offset = 0
-	
-	// Unmarshalling data ByteCount
-	if len(rawDataContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for ByteCount")
-	}
-	c.ByteCount = types.USHORT(binary.BigEndian.Uint16(rawDataContent[offset:offset+2]))
-	offset += 2
+	// No data is sent by this message.
 
 	return offset, nil
 }
