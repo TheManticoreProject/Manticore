b4cf46c98db9f12f6e0a6fd1c8386947899dd673
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-05 21:01:33 +0200

    Fixed LockAndReadResponse

--- network/smb/smb_v10/message/commands/LockAndReadResponse.go
+++ network/smb/smb_v10/message/commands/LockAndReadResponse.go
@@ -18,13 +18,20 @@ type LockAndReadResponse struct {
 	command_interface.Command
 
 	// Parameters
-	WordCount types.UCHAR
+
+	// CountOfBytesReturned (2 bytes): The actual number of bytes returned to the client. This MUST be equal to CountOfBytesToRead
+	// unless the end of file was reached before reading CountOfBytesToRead bytes or the ReadOffsetInBytes pointed at or beyond the end of file.
 	CountOfBytesReturned types.USHORT
 
-	// Data
-	BufferType types.UCHAR
-	CountOfBytesRead types.USHORT
+	// Reserved (8 bytes): Reserved. All bytes MUST be 0x00.
+	Reserved [4]types.USHORT
 
+	// Data
+
+	// BufferType (1 byte): This field MUST be 0x01.
+	// CountOfBytesRead (2 bytes): The number of bytes read that are contained in the following array of bytes.
+	// Bytes (variable): The array of bytes read from the file. The array is not null-terminated.
+	BytesRead types.SMB_STRING
 }
 
 // NewLockAndReadResponse creates a new LockAndReadResponse structure
@@ -34,21 +41,17 @@ type LockAndReadResponse struct {
 func NewLockAndReadResponse() *LockAndReadResponse {
 	c := &LockAndReadResponse{
 		// Parameters
-		WordCount: types.UCHAR(0),
 		CountOfBytesReturned: types.USHORT(0),
+		Reserved:             [4]types.USHORT{},
 
 		// Data
-		BufferType: types.UCHAR(0),
-		CountOfBytesRead: types.USHORT(0),
-
+		BytesRead: types.SMB_STRING{},
 	}
 
 	c.Command.SetCommandCode(codes.SMB_COM_LOCK_AND_READ)
 
 	return c
 }
-
-
 
 // Marshal marshals the LockAndReadResponse structure into a byte array
 //
@@ -83,26 +86,22 @@ func (c *LockAndReadResponse) Marshal() ([]byte, error) {
 	// This is because some parameters are dependent on the data, for example the size of some fields within
 	// the data will be stored in the parameters
 	rawDataContent := []byte{}
-	
-	// Marshalling data BufferType
-	rawDataContent = append(rawDataContent, types.UCHAR(c.BufferType))
-	
-	// Marshalling data CountOfBytesRead
-	buf2 := make([]byte, 2)
-	binary.BigEndian.PutUint16(buf2, uint16(c.CountOfBytesRead))
-	rawDataContent = append(rawDataContent, buf2...)
-	
+
+	// Marshalling data BytesRead
+	marshalledBytesRead, err := c.BytesRead.Marshal()
+	if err != nil {
+		return nil, err
+	}
+	rawDataContent = append(rawDataContent, marshalledBytesRead...)
+
 	// Then marshal the parameters
 	rawParametersContent := []byte{}
-	
-	// Marshalling parameter WordCount
-	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
-	
+
 	// Marshalling parameter CountOfBytesReturned
-	buf2 = make([]byte, 2)
+	buf2 := make([]byte, 2)
 	binary.BigEndian.PutUint16(buf2, uint16(c.CountOfBytesReturned))
 	rawParametersContent = append(rawParametersContent, buf2...)
-	
+
 	// Marshalling parameters
 	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
 	marshalledParameters, err := c.GetParameters().Marshal()
@@ -110,7 +109,7 @@ func (c *LockAndReadResponse) Marshal() ([]byte, error) {
 		return nil, err
 	}
 	marshalledCommand = append(marshalledCommand, marshalledParameters...)
-	
+
 	// Marshalling data
 	c.GetData().Add(rawDataContent)
 	marshalledData, err := c.GetData().Marshal()
@@ -146,37 +145,23 @@ func (c *LockAndReadResponse) Unmarshal(data []byte) (int, error) {
 
 	// First unmarshal the parameters
 	offset = 0
-	
-	// Unmarshalling parameter WordCount
-	if len(rawParametersContent) < offset+1 {
-	    return offset, fmt.Errorf("data too short for WordCount")
-	}
-	c.WordCount = types.UCHAR(rawParametersContent[offset])
-	offset++
-	
+
 	// Unmarshalling parameter CountOfBytesReturned
 	if len(rawParametersContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for CountOfBytesReturned")
+		return offset, fmt.Errorf("rawParametersContent too short for CountOfBytesReturned")
 	}
-	c.CountOfBytesReturned = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	c.CountOfBytesReturned = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset : offset+2]))
 	offset += 2
-	
+
 	// Then unmarshal the data
 	offset = 0
-	
-	// Unmarshalling data BufferType
-	if len(rawDataContent) < offset+1 {
-	    return offset, fmt.Errorf("rawParametersContent too short for BufferType")
+
+	// Unmarshalling data BytesRead
+	bytesRead, err = c.BytesRead.Unmarshal(rawDataContent)
+	if err != nil {
+		return 0, err
 	}
-	c.BufferType = types.UCHAR(rawDataContent[offset])
-	offset++
-	
-	// Unmarshalling data CountOfBytesRead
-	if len(rawDataContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for CountOfBytesRead")
-	}
-	c.CountOfBytesRead = types.USHORT(binary.BigEndian.Uint16(rawDataContent[offset:offset+2]))
-	offset += 2
+	offset += bytesRead
 
 	return offset, nil
 }
