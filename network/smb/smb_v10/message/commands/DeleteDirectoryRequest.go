f0e246e836fe1331ece1b248240624b4f6e29b86
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-05 11:11:44 +0200

    Fixed DeleteDirectoryRequest

--- network/smb/smb_v10/message/commands/DeleteDirectoryRequest.go
+++ network/smb/smb_v10/message/commands/DeleteDirectoryRequest.go
@@ -1,9 +1,6 @@
 package commands
 
 import (
-	"encoding/binary"
-	"fmt"
-
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
@@ -17,13 +14,8 @@ import (
 type DeleteDirectoryRequest struct {
 	command_interface.Command
 
-	// Parameters
-	WordCount types.UCHAR
-
 	// Data
-	BufferFormat types.UCHAR
 	DirectoryName types.SMB_STRING
-
 }
 
 // NewDeleteDirectoryRequest creates a new DeleteDirectoryRequest structure
@@ -32,21 +24,14 @@ type DeleteDirectoryRequest struct {
 // - A pointer to the new DeleteDirectoryRequest structure
 func NewDeleteDirectoryRequest() *DeleteDirectoryRequest {
 	c := &DeleteDirectoryRequest{
-		// Parameters
-		WordCount: types.UCHAR(0),
-
 		// Data
-		BufferFormat: types.UCHAR(0),
 		DirectoryName: types.SMB_STRING{},
-
 	}
 
 	c.Command.SetCommandCode(codes.SMB_COM_DELETE_DIRECTORY)
 
 	return c
 }
-
-
 
 // Marshal marshals the DeleteDirectoryRequest structure into a byte array
 //
@@ -81,23 +66,17 @@ func (c *DeleteDirectoryRequest) Marshal() ([]byte, error) {
 	// This is because some parameters are dependent on the data, for example the size of some fields within
 	// the data will be stored in the parameters
 	rawDataContent := []byte{}
-	
-	// Marshalling data BufferFormat
-	rawDataContent = append(rawDataContent, types.UCHAR(c.BufferFormat))
-	
+
 	// Marshalling data DirectoryName
 	bytesStream, err := c.DirectoryName.Marshal()
 	if err != nil {
-			return nil, err
+		return nil, err
 	}
 	rawDataContent = append(rawDataContent, bytesStream...)
-	
+
 	// Then marshal the parameters
 	rawParametersContent := []byte{}
-	
-	// Marshalling parameter WordCount
-	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
-	
+
 	// Marshalling parameters
 	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
 	marshalledParameters, err := c.GetParameters().Marshal()
@@ -105,7 +84,7 @@ func (c *DeleteDirectoryRequest) Marshal() ([]byte, error) {
 		return nil, err
 	}
 	marshalledCommand = append(marshalledCommand, marshalledParameters...)
-	
+
 	// Marshalling data
 	c.GetData().Add(rawDataContent)
 	marshalledData, err := c.GetData().Marshal()
@@ -132,7 +111,7 @@ func (c *DeleteDirectoryRequest) Unmarshal(data []byte) (int, error) {
 	if err != nil {
 		return 0, err
 	}
-	rawParametersContent := c.GetParameters().GetBytes()
+	_ = c.GetParameters().GetBytes()
 	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
@@ -141,28 +120,14 @@ func (c *DeleteDirectoryRequest) Unmarshal(data []byte) (int, error) {
 
 	// First unmarshal the parameters
 	offset = 0
-	
-	// Unmarshalling parameter WordCount
-	if len(rawParametersContent) < offset+1 {
-	    return offset, fmt.Errorf("data too short for WordCount")
-	}
-	c.WordCount = types.UCHAR(rawParametersContent[offset])
-	offset++
-	
+
 	// Then unmarshal the data
 	offset = 0
-	
-	// Unmarshalling data BufferFormat
-	if len(rawDataContent) < offset+1 {
-	    return offset, fmt.Errorf("rawParametersContent too short for BufferFormat")
-	}
-	c.BufferFormat = types.UCHAR(rawDataContent[offset])
-	offset++
-	
+
 	// Unmarshalling data DirectoryName
-	bytesRead, err := c.DirectoryName.Unmarshal(rawDataContent[offset:])
+	bytesRead, err = c.DirectoryName.Unmarshal(rawDataContent[offset:])
 	if err != nil {
-	    return offset, err
+		return offset, err
 	}
 	offset += bytesRead
 
