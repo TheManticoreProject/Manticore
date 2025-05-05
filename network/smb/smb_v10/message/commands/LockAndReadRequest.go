0853eda5f55c5aa116f3a4db1de64d30e519b0c2
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-05 20:58:06 +0200

    Fixed LockAndReadRequest

--- network/smb/smb_v10/message/commands/LockAndReadRequest.go
+++ network/smb/smb_v10/message/commands/LockAndReadRequest.go
@@ -18,15 +18,22 @@ type LockAndReadRequest struct {
 	command_interface.Command
 
 	// Parameters
-	WordCount types.UCHAR
+
+	// FID (2 bytes): This field MUST be a valid 16-bit unsigned integer indicating the file from which the data MUST be read.
 	FID types.USHORT
+
+	// CountOfBytesToRead (2 bytes): This field is a 16-bit unsigned integer indicating the number of bytes to be read from the file
+	// The client MUST ensure that the amount of data requested will fit in the negotiated maximum buffer size.
 	CountOfBytesToRead types.USHORT
+
+	// ReadOffsetInBytes (4 bytes): This field is a 32-bit unsigned integer indicating the offset in number of bytes from which to
+	// begin reading from the file. The client MUST ensure that the amount of data requested fits in the negotiated maximum buffer size.
+	// Because this field is limited to 32 bits, this command is inappropriate for files that have 64-bit offsets.
 	ReadOffsetInBytes types.ULONG
+
+	// EstimateOfRemainingBytesToBeRead (2 bytes): This field is a 16-bit unsigned integer indicating the remaining number of bytes that
+	// the client has designated to be read from the file. This is an advisory field and can be zero.
 	EstimateOfRemainingBytesToBeRead types.USHORT
-
-	// Data
-	ByteCount types.USHORT
-
 }
 
 // NewLockAndReadRequest creates a new LockAndReadRequest structure
@@ -36,23 +43,16 @@ type LockAndReadRequest struct {
 func NewLockAndReadRequest() *LockAndReadRequest {
 	c := &LockAndReadRequest{
 		// Parameters
-		WordCount: types.UCHAR(0),
-		FID: types.USHORT(0),
-		CountOfBytesToRead: types.USHORT(0),
-		ReadOffsetInBytes: types.ULONG(0),
+		FID:                              types.USHORT(0),
+		CountOfBytesToRead:               types.USHORT(0),
+		ReadOffsetInBytes:                types.ULONG(0),
 		EstimateOfRemainingBytesToBeRead: types.USHORT(0),
-
-		// Data
-		ByteCount: types.USHORT(0),
-
 	}
 
 	c.Command.SetCommandCode(codes.SMB_COM_LOCK_AND_READ)
 
 	return c
 }
-
-
 
 // Marshal marshals the LockAndReadRequest structure into a byte array
 //
@@ -87,38 +87,30 @@ func (c *LockAndReadRequest) Marshal() ([]byte, error) {
 	// This is because some parameters are dependent on the data, for example the size of some fields within
 	// the data will be stored in the parameters
 	rawDataContent := []byte{}
-	
-	// Marshalling data ByteCount
-	buf2 := make([]byte, 2)
-	binary.BigEndian.PutUint16(buf2, uint16(c.ByteCount))
-	rawDataContent = append(rawDataContent, buf2...)
-	
+
 	// Then marshal the parameters
 	rawParametersContent := []byte{}
-	
-	// Marshalling parameter WordCount
-	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
-	
+
 	// Marshalling parameter FID
-	buf2 = make([]byte, 2)
+	buf2 := make([]byte, 2)
 	binary.BigEndian.PutUint16(buf2, uint16(c.FID))
 	rawParametersContent = append(rawParametersContent, buf2...)
-	
+
 	// Marshalling parameter CountOfBytesToRead
 	buf2 = make([]byte, 2)
 	binary.BigEndian.PutUint16(buf2, uint16(c.CountOfBytesToRead))
 	rawParametersContent = append(rawParametersContent, buf2...)
-	
+
 	// Marshalling parameter ReadOffsetInBytes
 	buf4 := make([]byte, 4)
 	binary.BigEndian.PutUint32(buf4, uint32(c.ReadOffsetInBytes))
 	rawParametersContent = append(rawParametersContent, buf4...)
-	
+
 	// Marshalling parameter EstimateOfRemainingBytesToBeRead
 	buf2 = make([]byte, 2)
 	binary.BigEndian.PutUint16(buf2, uint16(c.EstimateOfRemainingBytesToBeRead))
 	rawParametersContent = append(rawParametersContent, buf2...)
-	
+
 	// Marshalling parameters
 	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
 	marshalledParameters, err := c.GetParameters().Marshal()
@@ -126,7 +118,7 @@ func (c *LockAndReadRequest) Marshal() ([]byte, error) {
 		return nil, err
 	}
 	marshalledCommand = append(marshalledCommand, marshalledParameters...)
-	
+
 	// Marshalling data
 	c.GetData().Add(rawDataContent)
 	marshalledData, err := c.GetData().Marshal()
@@ -158,55 +150,41 @@ func (c *LockAndReadRequest) Unmarshal(data []byte) (int, error) {
 	if err != nil {
 		return 0, err
 	}
-	rawDataContent := c.GetData().GetBytes()
+	_ = c.GetData().GetBytes()
 
 	// First unmarshal the parameters
 	offset = 0
-	
-	// Unmarshalling parameter WordCount
-	if len(rawParametersContent) < offset+1 {
-	    return offset, fmt.Errorf("data too short for WordCount")
-	}
-	c.WordCount = types.UCHAR(rawParametersContent[offset])
-	offset++
-	
+
 	// Unmarshalling parameter FID
 	if len(rawParametersContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for FID")
+		return offset, fmt.Errorf("rawParametersContent too short for FID")
 	}
-	c.FID = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	c.FID = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset : offset+2]))
 	offset += 2
-	
+
 	// Unmarshalling parameter CountOfBytesToRead
 	if len(rawParametersContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for CountOfBytesToRead")
+		return offset, fmt.Errorf("rawParametersContent too short for CountOfBytesToRead")
 	}
-	c.CountOfBytesToRead = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	c.CountOfBytesToRead = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset : offset+2]))
 	offset += 2
-	
+
 	// Unmarshalling parameter ReadOffsetInBytes
 	if len(rawParametersContent) < offset+4 {
-	    return offset, fmt.Errorf("rawParametersContent too short for ReadOffsetInBytes")
+		return offset, fmt.Errorf("rawParametersContent too short for ReadOffsetInBytes")
 	}
-	c.ReadOffsetInBytes = types.ULONG(binary.BigEndian.Uint32(rawParametersContent[offset:offset+4]))
+	c.ReadOffsetInBytes = types.ULONG(binary.BigEndian.Uint32(rawParametersContent[offset : offset+4]))
 	offset += 4
-	
+
 	// Unmarshalling parameter EstimateOfRemainingBytesToBeRead
 	if len(rawParametersContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for EstimateOfRemainingBytesToBeRead")
+		return offset, fmt.Errorf("rawParametersContent too short for EstimateOfRemainingBytesToBeRead")
 	}
-	c.EstimateOfRemainingBytesToBeRead = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	c.EstimateOfRemainingBytesToBeRead = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset : offset+2]))
 	offset += 2
-	
+
 	// Then unmarshal the data
 	offset = 0
-	
-	// Unmarshalling data ByteCount
-	if len(rawDataContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for ByteCount")
-	}
-	c.ByteCount = types.USHORT(binary.BigEndian.Uint16(rawDataContent[offset:offset+2]))
-	offset += 2
 
 	return offset, nil
 }
