ccb68b334cb16a4187358ef9b1c90e9be2005c2c
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 09:55:53 +0200

    Updated NtTransactResponse

--- network/smb/smb_v10/message/commands/NtTransactResponse.go
+++ network/smb/smb_v10/message/commands/NtTransactResponse.go
@@ -1,28 +1,17 @@
 package commands
 
 import (
-	"encoding/binary"
-	"fmt"
-
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/data"
 	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/parameters"
-	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/types"
 )
 
 // NtTransactResponse
 // Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/dd00c842-2398-412f-b21d-bf5074a9a1c4
 type NtTransactResponse struct {
 	command_interface.Command
-
-	// Parameters
-	WordCount types.UCHAR
-
-	// Data
-	ByteCount types.USHORT
-
 }
 
 // NewNtTransactResponse creates a new NtTransactResponse structure
@@ -30,21 +19,12 @@ type NtTransactResponse struct {
 // Returns:
 // - A pointer to the new NtTransactResponse structure
 func NewNtTransactResponse() *NtTransactResponse {
-	c := &NtTransactResponse{
-		// Parameters
-		WordCount: types.UCHAR(0),
-
-		// Data
-		ByteCount: types.USHORT(0),
-
-	}
+	c := &NtTransactResponse{}
 
 	c.Command.SetCommandCode(codes.SMB_COM_NT_TRANSACT)
 
 	return c
 }
-
-
 
 // Marshal marshals the NtTransactResponse structure into a byte array
 //
@@ -79,18 +59,10 @@ func (c *NtTransactResponse) Marshal() ([]byte, error) {
 	// This is because some parameters are dependent on the data, for example the size of some fields within
 	// the data will be stored in the parameters
 	rawDataContent := []byte{}
-	
-	// Marshalling data ByteCount
-	buf2 := make([]byte, 2)
-	binary.BigEndian.PutUint16(buf2, uint16(c.ByteCount))
-	rawDataContent = append(rawDataContent, buf2...)
-	
+
 	// Then marshal the parameters
 	rawParametersContent := []byte{}
-	
-	// Marshalling parameter WordCount
-	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
-	
+
 	// Marshalling parameters
 	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
 	marshalledParameters, err := c.GetParameters().Marshal()
@@ -98,7 +70,7 @@ func (c *NtTransactResponse) Marshal() ([]byte, error) {
 		return nil, err
 	}
 	marshalledCommand = append(marshalledCommand, marshalledParameters...)
-	
+
 	// Marshalling data
 	c.GetData().Add(rawDataContent)
 	marshalledData, err := c.GetData().Marshal()
@@ -125,32 +97,20 @@ func (c *NtTransactResponse) Unmarshal(data []byte) (int, error) {
 	if err != nil {
 		return 0, err
 	}
-	rawParametersContent := c.GetParameters().GetBytes()
+	_ = c.GetParameters().GetBytes()
 	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
-	rawDataContent := c.GetData().GetBytes()
+	_ = c.GetData().GetBytes()
 
 	// First unmarshal the parameters
 	offset = 0
-	
-	// Unmarshalling parameter WordCount
-	if len(rawParametersContent) < offset+1 {
-	    return offset, fmt.Errorf("data too short for WordCount")
-	}
-	c.WordCount = types.UCHAR(rawParametersContent[offset])
-	offset++
-	
+	// No parameters are sent in this message
+
 	// Then unmarshal the data
 	offset = 0
-	
-	// Unmarshalling data ByteCount
-	if len(rawDataContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for ByteCount")
-	}
-	c.ByteCount = types.USHORT(binary.BigEndian.Uint16(rawDataContent[offset:offset+2]))
-	offset += 2
+	// No data is sent in this message
 
 	return offset, nil
 }
