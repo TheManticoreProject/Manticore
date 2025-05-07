ee9b66e766f39ea482681ff5d3124e11f4ab804a
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 21:33:21 +0200

    Updated ProcessExitResponse

--- network/smb/smb_v10/message/commands/ProcessExitResponse.go
+++ network/smb/smb_v10/message/commands/ProcessExitResponse.go
@@ -98,7 +98,7 @@ func (c *ProcessExitResponse) Unmarshal(data []byte) (int, error) {
 		return 0, err
 	}
 	_ = c.GetParameters().GetBytes()
-	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	_, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
