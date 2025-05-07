620fe02a67bc34dce825e4e52d355355f302a17d
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 21:33:22 +0200

    Updated RenameRequest

--- network/smb/smb_v10/message/commands/RenameRequest.go
+++ network/smb/smb_v10/message/commands/RenameRequest.go
@@ -155,7 +155,7 @@ func (c *RenameRequest) Unmarshal(data []byte) (int, error) {
 		return 0, err
 	}
 	rawParametersContent := c.GetParameters().GetBytes()
-	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	_, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
