fa1e344b112a6c329504bc340fbf6d07a9175bb2
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 21:33:23 +0200

    Updated WriteAndUnlockResponse

--- network/smb/smb_v10/message/commands/WriteAndUnlockResponse.go
+++ network/smb/smb_v10/message/commands/WriteAndUnlockResponse.go
@@ -119,7 +119,7 @@ func (c *WriteAndUnlockResponse) Unmarshal(data []byte) (int, error) {
 		return 0, err
 	}
 	rawParametersContent := c.GetParameters().GetBytes()
-	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	_, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
