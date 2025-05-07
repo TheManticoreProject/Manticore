4aae217ea674765817aca4451181c3c52d37a25c
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 11:23:13 +0200

    Updated QueryInformationDiskResponse

--- network/smb/smb_v10/message/commands/QueryInformationDiskResponse.go
+++ network/smb/smb_v10/message/commands/QueryInformationDiskResponse.go
@@ -18,16 +18,26 @@ type QueryInformationDiskResponse struct {
 	command_interface.Command
 
 	// Parameters
-	WordCount types.UCHAR
+
+	// TotalUnits (2 bytes): This field is a 16-bit unsigned value that represents the
+	// total count of logical allocation units available on the volume.
 	TotalUnits types.USHORT
+
+	// BlocksPerUnit (2 bytes): This field is a 16-bit unsigned value that represents
+	// the number of blocks per allocation unit for the volume.
 	BlocksPerUnit types.USHORT
+
+	// BlockSize (2 bytes): This field is a 16-bit unsigned value that represents the
+	// size in bytes of each allocation unit for the volume.
 	BlockSize types.USHORT
+
+	// FreeUnits (2 bytes): This field is a 16-bit unsigned value that represents the
+	// total number of free allocation units available on the volume.
 	FreeUnits types.USHORT
+
+	// Reserved (2 bytes): This field is a 16-bit unsigned field and is reserved. The
+	// client SHOULD ignore this field.
 	Reserved types.USHORT
-
-	// Data
-	ByteCount types.USHORT
-
 }
 
 // NewQueryInformationDiskResponse creates a new QueryInformationDiskResponse structure
@@ -37,24 +47,18 @@ type QueryInformationDiskResponse struct {
 func NewQueryInformationDiskResponse() *QueryInformationDiskResponse {
 	c := &QueryInformationDiskResponse{
 		// Parameters
-		WordCount: types.UCHAR(0),
-		TotalUnits: types.USHORT(0),
+
+		TotalUnits:    types.USHORT(0),
 		BlocksPerUnit: types.USHORT(0),
-		BlockSize: types.USHORT(0),
-		FreeUnits: types.USHORT(0),
-		Reserved: types.USHORT(0),
-
-		// Data
-		ByteCount: types.USHORT(0),
-
+		BlockSize:     types.USHORT(0),
+		FreeUnits:     types.USHORT(0),
+		Reserved:      types.USHORT(0),
 	}
 
 	c.Command.SetCommandCode(codes.SMB_COM_QUERY_INFORMATION_DISK)
 
 	return c
 }
-
-
 
 // Marshal marshals the QueryInformationDiskResponse structure into a byte array
 //
@@ -89,43 +93,35 @@ func (c *QueryInformationDiskResponse) Marshal() ([]byte, error) {
 	// This is because some parameters are dependent on the data, for example the size of some fields within
 	// the data will be stored in the parameters
 	rawDataContent := []byte{}
-	
-	// Marshalling data ByteCount
-	buf2 := make([]byte, 2)
-	binary.BigEndian.PutUint16(buf2, uint16(c.ByteCount))
-	rawDataContent = append(rawDataContent, buf2...)
-	
+
 	// Then marshal the parameters
 	rawParametersContent := []byte{}
-	
-	// Marshalling parameter WordCount
-	rawParametersContent = append(rawParametersContent, types.UCHAR(c.WordCount))
-	
+
 	// Marshalling parameter TotalUnits
-	buf2 = make([]byte, 2)
+	buf2 := make([]byte, 2)
 	binary.BigEndian.PutUint16(buf2, uint16(c.TotalUnits))
 	rawParametersContent = append(rawParametersContent, buf2...)
-	
+
 	// Marshalling parameter BlocksPerUnit
 	buf2 = make([]byte, 2)
 	binary.BigEndian.PutUint16(buf2, uint16(c.BlocksPerUnit))
 	rawParametersContent = append(rawParametersContent, buf2...)
-	
+
 	// Marshalling parameter BlockSize
 	buf2 = make([]byte, 2)
 	binary.BigEndian.PutUint16(buf2, uint16(c.BlockSize))
 	rawParametersContent = append(rawParametersContent, buf2...)
-	
+
 	// Marshalling parameter FreeUnits
 	buf2 = make([]byte, 2)
 	binary.BigEndian.PutUint16(buf2, uint16(c.FreeUnits))
 	rawParametersContent = append(rawParametersContent, buf2...)
-	
+
 	// Marshalling parameter Reserved
 	buf2 = make([]byte, 2)
 	binary.BigEndian.PutUint16(buf2, uint16(c.Reserved))
 	rawParametersContent = append(rawParametersContent, buf2...)
-	
+
 	// Marshalling parameters
 	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
 	marshalledParameters, err := c.GetParameters().Marshal()
@@ -133,7 +129,7 @@ func (c *QueryInformationDiskResponse) Marshal() ([]byte, error) {
 		return nil, err
 	}
 	marshalledCommand = append(marshalledCommand, marshalledParameters...)
-	
+
 	// Marshalling data
 	c.GetData().Add(rawDataContent)
 	marshalledData, err := c.GetData().Marshal()
@@ -165,62 +161,49 @@ func (c *QueryInformationDiskResponse) Unmarshal(data []byte) (int, error) {
 	if err != nil {
 		return 0, err
 	}
-	rawDataContent := c.GetData().GetBytes()
+	_ = c.GetData().GetBytes()
 
 	// First unmarshal the parameters
 	offset = 0
-	
-	// Unmarshalling parameter WordCount
-	if len(rawParametersContent) < offset+1 {
-	    return offset, fmt.Errorf("data too short for WordCount")
-	}
-	c.WordCount = types.UCHAR(rawParametersContent[offset])
-	offset++
-	
+
 	// Unmarshalling parameter TotalUnits
 	if len(rawParametersContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for TotalUnits")
+		return offset, fmt.Errorf("rawParametersContent too short for TotalUnits")
 	}
-	c.TotalUnits = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	c.TotalUnits = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset : offset+2]))
 	offset += 2
-	
+
 	// Unmarshalling parameter BlocksPerUnit
 	if len(rawParametersContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for BlocksPerUnit")
+		return offset, fmt.Errorf("rawParametersContent too short for BlocksPerUnit")
 	}
-	c.BlocksPerUnit = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	c.BlocksPerUnit = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset : offset+2]))
 	offset += 2
-	
+
 	// Unmarshalling parameter BlockSize
 	if len(rawParametersContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for BlockSize")
+		return offset, fmt.Errorf("rawParametersContent too short for BlockSize")
 	}
-	c.BlockSize = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	c.BlockSize = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset : offset+2]))
 	offset += 2
-	
+
 	// Unmarshalling parameter FreeUnits
 	if len(rawParametersContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for FreeUnits")
+		return offset, fmt.Errorf("rawParametersContent too short for FreeUnits")
 	}
-	c.FreeUnits = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	c.FreeUnits = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset : offset+2]))
 	offset += 2
-	
+
 	// Unmarshalling parameter Reserved
 	if len(rawParametersContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for Reserved")
+		return offset, fmt.Errorf("rawParametersContent too short for Reserved")
 	}
-	c.Reserved = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset:offset+2]))
+	c.Reserved = types.USHORT(binary.BigEndian.Uint16(rawParametersContent[offset : offset+2]))
 	offset += 2
-	
+
 	// Then unmarshal the data
 	offset = 0
-	
-	// Unmarshalling data ByteCount
-	if len(rawDataContent) < offset+2 {
-	    return offset, fmt.Errorf("rawParametersContent too short for ByteCount")
-	}
-	c.ByteCount = types.USHORT(binary.BigEndian.Uint16(rawDataContent[offset:offset+2]))
-	offset += 2
+	// No data is sent in this message
 
 	return offset, nil
 }
