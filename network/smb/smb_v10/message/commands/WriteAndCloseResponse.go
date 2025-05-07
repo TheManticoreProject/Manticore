0f6f6c11b9dd06e8dd47d26c2f074ab5a2cba1f5
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 21:33:23 +0200

    Updated WriteAndCloseResponse

--- network/smb/smb_v10/message/commands/WriteAndCloseResponse.go
+++ network/smb/smb_v10/message/commands/WriteAndCloseResponse.go
@@ -119,7 +119,7 @@ func (c *WriteAndCloseResponse) Unmarshal(data []byte) (int, error) {
 		return 0, err
 	}
 	rawParametersContent := c.GetParameters().GetBytes()
-	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	_, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
