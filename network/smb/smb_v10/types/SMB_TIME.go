7706f3c6fccd97bab39fc07533da2f078f5af39a
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 22:05:08 +0200

    Updated SMB_TIME

--- network/smb/smb_v10/types/SMB_TIME.go
+++ network/smb/smb_v10/types/SMB_TIME.go
@@ -1,5 +1,20 @@
 package types
 
-import "github.com/TheManticoreProject/Manticore/windows/ms_dtyp/common/data_structures"
+import (
+	"time"
+
+	"github.com/TheManticoreProject/Manticore/windows/ms_dtyp/common/data_structures"
+)
 
 type SMB_TIME = data_structures.FILETIME
+
+// NewSMB_TIMEFromTime creates a new SMB_TIME from a time.Time
+//
+// Parameters:
+// - t: The time.Time to create the SMB_TIME from
+//
+// Returns:
+// - The new SMB_TIME
+func NewSMB_TIMEFromTime(t time.Time) *SMB_TIME {
+	return data_structures.NewFILETIMEFromTime(t)
+}
