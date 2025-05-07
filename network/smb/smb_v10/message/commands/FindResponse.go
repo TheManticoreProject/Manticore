8a72a25a049ce54cac9ffd44c454dfeb731c2aa1
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 21:33:20 +0200

    Updated FindResponse

--- network/smb/smb_v10/message/commands/FindResponse.go
+++ network/smb/smb_v10/message/commands/FindResponse.go
@@ -137,7 +137,7 @@ func (c *FindResponse) Unmarshal(data []byte) (int, error) {
 		return 0, err
 	}
 	rawParametersContent := c.GetParameters().GetBytes()
-	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	_, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
