899d45a88af5ece5603f388133e431ba34e3e978
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 13:47:19 +0200

    Updated ReadRawResponse

--- network/smb/smb_v10/message/commands/ReadRawResponse.go
+++ network/smb/smb_v10/message/commands/ReadRawResponse.go
@@ -1,130 +1,25 @@
 package commands
 
-import (
-	"encoding/binary"
-	"fmt"
-
-	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/andx"
-	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/codes"
-	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/commands/command_interface"
-	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/data"
-	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/message/parameters"
-	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/types"
-)
-
 // ReadRawResponse
 // Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/3f3914f6-d251-48ab-89c0-35349cb6d1c8
-type ReadRawResponse struct {
-	command_interface.Command
 
-	// Parameters
+// The server's "raw" response
+// - No SMB Header at all. Instead, the server uses the underlying SMB transport's session-service framing
+// (4-byte NetBIOS header: 1-byte MessageType + 3-byte Length) and then emits only the file-data bytes. There is:
+// [NetBIOS Session Service header:
+//     - MessageType = 0x00 (session message)
+//     - Length = N (the number of data bytes)
+// ]
+// [Raw data payload:  N bytes of file or pipe data]
 
-	// Data
+// There is no SMB Status/Flags fields, no WordCount, no DataOffset-just a single contiguous byte-stream whose length
+// is inferred from the transport header.
+// Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-cifs/07e4a838-0137-4405-b534-1e5eeaf16812
+// - If the server can't fulfill the read (error or EOF), it simply returns a zero-length payload (Length=0)
 
-}
+// Why "no SMB Header" on the response?
 
-// NewReadRawResponse creates a new ReadRawResponse structure
-//
-// Returns:
-// - A pointer to the new ReadRawResponse structure
-func NewReadRawResponse() *ReadRawResponse {
-	c := &ReadRawResponse{
-		// Parameters
-
-		// Data
-
-	}
-
-	c.Command.SetCommandCode(codes.SMB_COM_READ_RAW)
-
-	return c
-}
-
-
-
-// Marshal marshals the ReadRawResponse structure into a byte array
-//
-// Returns:
-// - A byte array representing the ReadRawResponse structure
-// - An error if the marshaling fails
-func (c *ReadRawResponse) Marshal() ([]byte, error) {
-	marshalledCommand := []byte{}
-
-	// Create the Parameters structure if it is nil
-	if c.GetParameters() == nil {
-		c.SetParameters(parameters.NewParameters())
-	}
-	// Create the Data structure if it is nil
-	if c.GetData() == nil {
-		c.SetData(data.NewData())
-	}
-
-	// In case of AndX, we need to add the parameters to the Parameters structure first
-	if c.IsAndX() {
-		if c.GetAndX() == nil {
-			c.SetAndX(andx.NewAndX())
-			c.GetAndX().AndXCommand = c.GetCommandCode()
-		}
-
-		for _, parameter := range c.GetAndX().GetParameters() {
-			c.GetParameters().AddWord(parameter)
-		}
-	}
-
-	// First marshal the data and then the parameters
-	// This is because some parameters are dependent on the data, for example the size of some fields within
-	// the data will be stored in the parameters
-	rawDataContent := []byte{}
-	
-	// Then marshal the parameters
-	rawParametersContent := []byte{}
-	
-	// Marshalling parameters
-	c.GetParameters().AddWordsFromBytesStream(rawParametersContent)
-	marshalledParameters, err := c.GetParameters().Marshal()
-	if err != nil {
-		return nil, err
-	}
-	marshalledCommand = append(marshalledCommand, marshalledParameters...)
-	
-	// Marshalling data
-	c.GetData().Add(rawDataContent)
-	marshalledData, err := c.GetData().Marshal()
-	if err != nil {
-		return nil, err
-	}
-	marshalledCommand = append(marshalledCommand, marshalledData...)
-
-	return marshalledCommand, nil
-}
-
-// Unmarshal unmarshals a byte array into the command structure
-//
-// Parameters:
-// - data: The byte array to unmarshal
-//
-// Returns:
-// - The number of bytes unmarshalled
-func (c *ReadRawResponse) Unmarshal(data []byte) (int, error) {
-	offset := 0
-
-	// First unmarshal the two structures
-	bytesRead, err := c.GetParameters().Unmarshal(data)
-	if err != nil {
-		return 0, err
-	}
-	rawParametersContent := c.GetParameters().GetBytes()
-	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
-	if err != nil {
-		return 0, err
-	}
-	rawDataContent := c.GetData().GetBytes()
-
-	// First unmarshal the parameters
-	offset = 0
-	
-	// Then unmarshal the data
-	offset = 0
-
-	return offset, nil
-}
+// Because SMB_COM_READ_RAW is optimized for bulk reads, the designers stripped away the per-message header to avoid
+// extra framing overhead and allow messages larger than the negotiated buffer size. The only framing is done by the
+// NetBIOS session layer, so the client must quiesce all other requests until it has received exactly the number of
+// bytes (or fewer, on EOF) that it asked for
