d1a7a63072724304607cb0ab1792b37ed9970e82
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 21:33:23 +0200

    Updated WriteRawRequest

--- network/smb/smb_v10/message/commands/WriteRawRequest.go
+++ network/smb/smb_v10/message/commands/WriteRawRequest.go
@@ -239,7 +239,7 @@ func (c *WriteRawRequest) Unmarshal(data []byte) (int, error) {
 		return 0, err
 	}
 	rawParametersContent := c.GetParameters().GetBytes()
-	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	_, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
