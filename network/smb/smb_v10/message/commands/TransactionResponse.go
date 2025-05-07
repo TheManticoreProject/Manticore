7c2b35af0f8a9231164bc263b334bd22bd3e2d1b
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 21:33:22 +0200

    Updated TransactionResponse

--- network/smb/smb_v10/message/commands/TransactionResponse.go
+++ network/smb/smb_v10/message/commands/TransactionResponse.go
@@ -104,7 +104,7 @@ func (c *TransactionResponse) Unmarshal(data []byte) (int, error) {
 		return 0, err
 	}
 	_ = c.GetParameters().GetBytes()
-	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	_, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
