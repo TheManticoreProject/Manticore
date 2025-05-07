9968e3f1cd2669663b54a5884f6fe3ac3044313d
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 21:33:20 +0200

    Updated IoctlResponse

--- network/smb/smb_v10/message/commands/IoctlResponse.go
+++ network/smb/smb_v10/message/commands/IoctlResponse.go
@@ -234,7 +234,7 @@ func (c *IoctlResponse) Unmarshal(data []byte) (int, error) {
 		return 0, err
 	}
 	rawParametersContent := c.GetParameters().GetBytes()
-	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	_, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
