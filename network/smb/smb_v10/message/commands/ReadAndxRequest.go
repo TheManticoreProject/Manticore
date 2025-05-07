46b662cd7eb225a67aa2bde8b51ea0c9a079f6fd
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 21:33:21 +0200

    Updated ReadAndxRequest

--- network/smb/smb_v10/message/commands/ReadAndxRequest.go
+++ network/smb/smb_v10/message/commands/ReadAndxRequest.go
@@ -176,7 +176,7 @@ func (c *ReadAndxRequest) Unmarshal(data []byte) (int, error) {
 		return 0, err
 	}
 	rawParametersContent := c.GetParameters().GetBytes()
-	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	_, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
