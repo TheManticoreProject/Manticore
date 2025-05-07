cd526acefcb1d54e42b1ed848bdd14d2386bc243
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-07 21:33:22 +0200

    Updated SearchResponse

--- network/smb/smb_v10/message/commands/SearchResponse.go
+++ network/smb/smb_v10/message/commands/SearchResponse.go
@@ -140,7 +140,7 @@ func (c *SearchResponse) Unmarshal(data []byte) (int, error) {
 		return 0, err
 	}
 	rawParametersContent := c.GetParameters().GetBytes()
-	bytesRead, err = c.GetData().Unmarshal(data[bytesRead:])
+	_, err = c.GetData().Unmarshal(data[bytesRead:])
 	if err != nil {
 		return 0, err
 	}
